b765e7fefd568922f349b9d9146d11c742a620bd
Sachin <sachin-shivakumar@users.noreply.github.com>
2022-06-14 18:35:24 -0700

    Update subsref

---
+++ PIETOOLS_2021b/opvar/@dopvar/numArgumentsFromSubscript.m
@@ -0,0 +1,10 @@
+function n = numArgumentsFromSubscript(obj,s,indexingContext)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This method is automatically called when one of the following statements
+% are executed:
+% a) obj.s — Number of elements referenced in a statement (indexingContext = Statement) 
+% b) (obj.s) — Number of elements returned in an expression (indexingContext = Expression) 
+% c) [obj.s] = rhs — Number of values assigned with a comma-separated list (indexingContext = Assignment)
+
+   n=1;
+end
