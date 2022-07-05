c7375450feb5f430d328e1eef494dfc3764a7e81
Sachin <sachin-shivakumar@users.noreply.github.com>
2022-07-04 17:15:35 -0700

    update int

--- PIETOOLS_2021b/parser/@state/int.m
+++ PIETOOLS_2021b/parser/@state/int.m
@@ -4,20 +4,30 @@ if any(isequal(lim,var))
     error('Limits of integration and integration variable are the same. Change the variable naming');
 end
 
-opvar T;
-if poly2double(lim(2))
-T.R.R2 = eye(sum(objC.veclength));
-end
-if poly2double(lim(1))
-T.R.R1 = eye(sum(objC.veclength));    
+if ~poly2double(lim(2))&&~poly2double(lim(1))
+    error('Integral should have at least one limit of integration as 0 or 1');
 end
 
+opvar T;
+if poly2double(lim(2))
+    if (double(lim(2))==1)
+        T.R.R2 = eye(sum(objC.veclength));
+    else 
+        error('Upper limit of integration can only be a "pvar" variable or 1');
+    end
+end
+if poly2double(lim(1))  
+    if (double(lim(1))==0)
+        T.R.R1 = eye(sum(objC.veclength));    
+    else
+        error('Lower limit of integration can only be a "pvar" variable or 0');
+    end
+end
 for i=1:length(objC)
     idx = find(isequal(objC(i).var,var));
     if ~isempty(idx)
         objC(i).delta_val(idx) = pvar('theta');
     end
 end
-
 prodTerms = terms(T,objC);
 end
