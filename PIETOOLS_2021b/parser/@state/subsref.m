d479cdfc581c64ff38812afaa14527da76b8b7d0
Sachin <sachin-shivakumar@users.noreply.github.com>
2022-07-17 17:34:35 -0700

    Update subsref

--- PIETOOLS_2021b/parser/@state/subsref.m
+++ PIETOOLS_2021b/parser/@state/subsref.m
@@ -1,32 +1,75 @@
 function out = subsref(obj,s)
 switch s(1).type
     case '.'
-        if strcmp(s(1).subs,'type')
-            for i=1:length(obj)
-                out{i,1} = obj(i).type;
+        if length(s) == 1
+            % Implement obj.PropertyName
+            if strcmp(s(1).subs,'type')
+                for i=1:length(obj)
+                    out{i,1} = obj(i).type;
+                end
+            elseif strcmp(s(1).subs,'var')
+                for i=1:length(obj)
+                    out{i,1} = obj(i).var;
+                end
+            elseif strcmp(s(1).subs,'veclength')
+                for i=1:length(obj)
+                    out(i,1) = obj(i).veclength;
+                end
+            elseif strcmp(s(1).subs,'diff_order')
+                for i=1:length(obj)
+                    out{i,1} = obj(i).diff_order;
+                end
+            elseif strcmp(s(1).subs,'statename')
+                for i=1:length(obj)
+                    out(i,1) = obj(i).statename;
+                end
+            else
+                error('Inaccessible/unknown property for the state object');
             end
-        elseif strcmp(s(1).subs,'var')
-            for i=1:length(obj)
-                out{i,1} = obj(i).var;
-            end
-        elseif strcmp(s(1).subs,'veclength')
-            for i=1:length(obj)
-                out(i,1) = obj(i).veclength;
-            end
-        elseif strcmp(s(1).subs,'diff_order')
-            for i=1:length(obj)
-                out{i,1} = obj(i).diff_order;
-            end
-        elseif strcmp(s(1).subs,'statename')
-            for i=1:length(obj)
-                out(i,1) = obj(i).statename;
+        elseif length(s) == 2 && strcmp(s(2).type,'()')
+            % Implement obj.PropertyName(indices)
+            if strcmp(s(1).subs,'var')
+                for i=1:length(obj)
+                    out(i,1) = obj(i).var(s(2).subs);
+                end
+            elseif strcmp(s(1).subs,'diff_order')
+                for i=1:length(obj)
+                    out(i,1) = obj(i).diff_order(s(2).subs);
+                end
+            else
+                error('Inaccessible/unknown property for the state object');
             end
         else
-            error('Inaccessible/unknown property for the state object');
+        % Use built-in for any other expression
+            error('Invalid indexing operation on state objec');
         end
     case '()'
-        out = builtin('subsref',obj,s);
-    case '{}'
-        error('Not a valid indexing expression')
-end
+        if length(s) == 1
+            % Implement obj(indices)
+            out = obj(s(1).subs);
+        elseif length(s) == 2 && strcmp(s(2).type,'.')
+        % Implement obj(ind).PropertyName
+            if strcmp(s(2).subs,'type')
+               out = obj(s(1).subs).type;
+            elseif strcmp(s(2).subs,'var')
+                out = obj(s(1).subs).var;
+            elseif strcmp(s(2).subs,'veclength')
+                out = obj(s(1).subs).veclength;
+            elseif strcmp(s(2).subs,'diff_order')
+                out = obj(s(1).subs).diff_order;
+            elseif strcmp(s(2).subs,'statename')
+                out = obj(s(1).subs).statename;
+            else
+                error('Inaccessible/unknown property for the state object');
+            end
+        elseif length(s) == 3 && strcmp(s(2).type,'.') && strcmp(s(3).type,'()')
+        % Implement obj(indices).PropertyName(indices)
+            out = obj(s(1).subs).(s(2).subs(:));
+            out = out(s(3).subs);
+        else
+        % Use built-in for any other expression
+            error('Invalid indexing operation on state objec');
+        end
+    otherwise
+        error('Invalid indexing operation on state objec');
 end
