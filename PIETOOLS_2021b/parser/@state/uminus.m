9b700eff83b39af4570eae877a559f6e49edc50a
Sachin <sachin-shivakumar@users.noreply.github.com>
2022-07-24 14:53:36 -0700

    Bug fixes for state methods
    Added checks to avoid int, mtimes, plus of vectors with time-derivatives and outputs

--- PIETOOLS_2021b/parser/@state/uminus.m
+++ PIETOOLS_2021b/parser/@state/uminus.m
@@ -1,5 +1,5 @@
 function sumTerms = uminus(objA)
 s.type = '.'; s.subs = 'veclength';
-opvar T; T.R.R0 = -eye(subsref(objA,s));
+opvar T; T.R.R0 = -eye(sum(subsref(objA,s)));
 sumTerms = terms(T,objA);
 end
--- PIETOOLS_2021b/parser/@state/uplus.m
+++ PIETOOLS_2021b/parser/@state/uplus.m
@@ -1,5 +1,5 @@
 function sumTerms = uplus(objA)
 s.type = '.'; s.subs = 'veclength';
-opvar T; T.R.R0 = eye(subsref(objA,s));
+opvar T; T.R.R0 = eye(sum(subsref(objA,s)));
 sumTerms = terms(T,objA);
 end
