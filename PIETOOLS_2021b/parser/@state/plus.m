50c7aeb7330e779f3d768829122b7979e3300cb5
Sachin <sachin-shivakumar@users.noreply.github.com>
2022-07-24 14:24:30 -0700

    plus bug fix

--- PIETOOLS_2021b/parser/@state/plus.m
+++ PIETOOLS_2021b/parser/@state/plus.m
@@ -9,12 +9,24 @@ s.type = '.'; s.subs = 'veclength';
 if sum(subsref(objA,s))~=sum(subsref(objB,s))
     error('States of unequal length cannot be added');
 end
-
+isdot_A = []; isdot_B = []; isout_A=[]; isout_B = [];
 for i=1:length(objA)
-    if (objA(i).diff_order(1))&&(objB(i).diff_order(1))
-        error("Two states with time derivatives cannot be added together");
-    end
+    isdot_A = [isdot_A; objA(i).diff_order(1)*ones(subsref(objA(i),s),1)];
+    isout_A = [isout_A; strcmp(objA(i).type,'out')*ones(subsref(objA(i),s),1)];
 end
+for i=1:length(objB)
+    isdot_B = [isdot_B; objB(i).diff_order(1)*ones(subsref(objB(i),s),1)];
+    isout_B = [isout_B; strcmp(objB(i).type,'out')*ones(subsref(objB(i),s),1)];
+end
+if any((isdot_A)&(isdot_B))
+    error("Two states with time derivatives cannot be added together");
+end
+
+if any((isdot_A&isout_B)|(isout_A&isdot_B)|(isout_A&isout_B))
+    error("Addition of two outputs or an output and time-derivative of state is not allowed");
+end
+
+
 
 [objC,permMatsA,permMatsB] = combine(objA,objB); % objA = permMats{1}*objC and objB = permMats{2}*objC
 
