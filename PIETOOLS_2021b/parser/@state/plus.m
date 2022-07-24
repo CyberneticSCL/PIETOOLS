2eee87d30c05e818c5e75890b8bfee390723feb8
Sachin <sachin-shivakumar@users.noreply.github.com>
2022-07-24 14:25:54 -0700

    Update plus.m

--- PIETOOLS_2021b/parser/@state/plus.m
+++ PIETOOLS_2021b/parser/@state/plus.m
@@ -9,6 +9,7 @@ s.type = '.'; s.subs = 'veclength';
 if sum(subsref(objA,s))~=sum(subsref(objB,s))
     error('States of unequal length cannot be added');
 end
+
 isdot_A = []; isdot_B = []; isout_A=[]; isout_B = [];
 for i=1:length(objA)
     isdot_A = [isdot_A; objA(i).diff_order(1)*ones(subsref(objA(i),s),1)];
@@ -18,12 +19,8 @@ for i=1:length(objB)
     isdot_B = [isdot_B; objB(i).diff_order(1)*ones(subsref(objB(i),s),1)];
     isout_B = [isout_B; strcmp(objB(i).type,'out')*ones(subsref(objB(i),s),1)];
 end
-if any((isdot_A)&(isdot_B))
-    error("Two states with time derivatives cannot be added together");
-end
-
-if any((isdot_A&isout_B)|(isout_A&isdot_B)|(isout_A&isout_B))
-    error("Addition of two outputs or an output and time-derivative of state is not allowed");
+if any((isdot_A&isout_B)|(isout_A&isdot_B)|(isout_A&isout_B)|(isdot_A&isdot_B))
+    error("Any linear combinations of outputs and time-derivative of state are not allowed");
 end
 
 
