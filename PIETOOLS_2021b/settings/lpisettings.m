3796562128a3c43411005950d6dd48f5f62396e8
Declan <91631433+djagt@users.noreply.github.com>
2022-08-11 19:52:53 -0700

    Adjust lpisettings.
    The "dim" argument has been removed from the function, since it outputs both the 1D and 2D settings anyway.

--- PIETOOLS_2021b/settings/lpisettings.m
+++ PIETOOLS_2021b/settings/lpisettings.m
@@ -1,4 +1,4 @@
-function sttngs = lpisettings(type,dim,derivative_strictness,simplify,solver)
+function sttngs = lpisettings(type,derivative_strictness,simplify,solver)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % sttngs = lpisettings(type,dim,derivative_strictness,simplify,solver)
 % defines a "settings" structure for the PIETOOLS executive functions,
@@ -13,12 +13,6 @@ function sttngs = lpisettings(type,dim,derivative_strictness,simplify,solver)
 %           complexity increasing as:
 %               extreme < stripped < light < heavy < veryheavy
 %           Defaults to 'light'.
-%   dim:    Scalar 1 or 2, indicating the dimension of the PIE for which to
-%           solve an LPI. If dim=1, 2d settings will be added as an
-%           additional field "settings_2d" to the output structure. If
-%           dim=2, 1d settings will be added as an additional field
-%           "settings_1d" to the output structure. 
-%           Defaults to 1.
 %   derivative_strictness:  Nonnegative scalar quantifying how strictly
 %                           negative the derivative of the LF in the
 %                           different executive functions is supposed to
@@ -71,88 +65,47 @@ function sttngs = lpisettings(type,dim,derivative_strictness,simplify,solver)
 
 arguments
     type {mustBeMember(type,{'light','heavy','veryheavy','stripped','extreme','custom'})}='light';
-    dim {mustBeMember(dim,[1:2])} = 1;
     derivative_strictness {mustBeNonnegative}= 0;
     simplify {mustBeMember(simplify,{'','psimplify'})}= '';
     solver {mustBeMember(solver,{'sedumi','mosek','sdpnalplus','sdpt3'})} = 'sedumi';
 end
 
-if dim==1
-    switch type
-        % Set the general LPI settings for 1D case, adding 2D settings as a
-        % separate field.
-        case 'light'
-            sttngs = settings_PIETOOLS_light;
-            sttngs.settings_2d = settings_PIETOOLS_light_2D;
-        case 'custom'
-            sttngs = settings_PIETOOLS_custom;
-            sttngs.settings_2d = settings_PIETOOLS_custom_2D;
-        case 'extreme'
-            sttngs = settings_PIETOOLS_extreme;      
-            sttngs.settings_2d = settings_PIETOOLS_extreme_2D;
-        case 'heavy'
-            sttngs = settings_PIETOOLS_heavy;
-            sttngs.settings_2d = settings_PIETOOLS_heavy_2D;
-        case 'stripped'
-            sttngs = settings_PIETOOLS_stripped;
-            sttngs.settings_2d = settings_PIETOOLS_stripped_2D;
-        case 'veryheavy'
-            sttngs = settings_PIETOOLS_veryheavy;
-            sttngs.settings_2d = settings_PIETOOLS_veryheavy_2D;
-        otherwise
-            warning('Unknown settings parameter requested. Defaulting to light settings');
-            sttngs = settings_PIETOOLS_light;
-            sttngs.settings_2d = settings_PIETOOLS_light_2D;
-    end
-    
-    % Set strictness of positivity of LF and negativity of its derivative.    
-    sttngs.eppos = 1e-4;      % Positivity of Lyapunov Function with respect to real-valued states
-    sttngs.eppos2 = 1*1e-6;   % Positivity of Lyapunov Function with respect to spatially distributed states
-    sttngs.epneg = derivative_strictness;    % Negativity of Derivative of Lyapunov Function in both ODE and PDE state
-    
-    % Set same settings for 2D case.
-    sttngs.settings_2d.eppos = [1e-4; 1e-6; 1e-6; 1e-6];    % Positivity of LF wrt R x L2[s1] x L2[s2] x L2[s1,s2]
-    sttngs.settings_2d.epneg = derivative_strictness;       % Negativity of Derivative of Lyapunov Function in both ODE and PDE state
-     
-    
-else
-    switch type
-        % Set the general LPI settings for 2D case, adding 1D settings as a
-        % separate field.
-        case 'light'
-            sttngs = settings_PIETOOLS_light_2D;
-            sttngs.settings_1d = settings_PIETOOLS_light;
-        case 'custom'
-            sttngs = settings_PIETOOLS_custom_2D;
-            sttngs.settings_1d = settings_PIETOOLS_custom;
-        case 'extreme'
-            sttngs = settings_PIETOOLS_extreme_2D;      
-            sttngs.settings_1d = settings_PIETOOLS_extreme;
-        case 'heavy'
-            sttngs = settings_PIETOOLS_heavy_2D;
-            sttngs.settings_1d = settings_PIETOOLS_heavy;
-        case 'stripped'
-            sttngs = settings_PIETOOLS_stripped_2D;
-            sttngs.settings_1d = settings_PIETOOLS_stripped;
-        case 'veryheavy'
-            sttngs = settings_PIETOOLS_veryheavy_2D;
-            sttngs.settings_1d = settings_PIETOOLS_veryheavy;
-        otherwise
-            warning('Unknown settings parameter requested. Defaulting to light settings');
-            sttngs = settings_PIETOOLS_light_2D;
-            sttngs.settings_1d = settings_PIETOOLS_light;
-    end
-    
-    % Set strictness of positivity of LF and negativity of its derivative.  
-    sttngs.eppos = [1e-4; 1e-6; 1e-6; 1e-6];    % Positivity of LF wrt R x L2[s1] x L2[s2] x L2[s1,s2]
-    sttngs.epneg = derivative_strictness;       % Negativity of Derivative of Lyapunov Function in both ODE and PDE state
-    
-    % Set same settings for 1D case.
-    sttngs.settings_1d.eppos = 1e-4;      % Positivity of Lyapunov Function with respect to real-valued states
-    sttngs.settings_1d.eppos2 = 1*1e-6;   % Positivity of Lyapunov Function with respect to spatially distributed states
-    sttngs.settings_1d.epneg = derivative_strictness;    % Negativity of Derivative of Lyapunov Function in both ODE and PDE state 
-    
+switch type
+    % Set the general LPI settings for 1D case, adding 2D settings as a
+    % separate field.
+    case 'light'
+        sttngs = settings_PIETOOLS_light;
+        sttngs.settings_2d = settings_PIETOOLS_light_2D;
+    case 'custom'
+        sttngs = settings_PIETOOLS_custom;
+        sttngs.settings_2d = settings_PIETOOLS_custom_2D;
+    case 'extreme'
+        sttngs = settings_PIETOOLS_extreme;
+        sttngs.settings_2d = settings_PIETOOLS_extreme_2D;
+    case 'heavy'
+        sttngs = settings_PIETOOLS_heavy;
+        sttngs.settings_2d = settings_PIETOOLS_heavy_2D;
+    case 'stripped'
+        sttngs = settings_PIETOOLS_stripped;
+        sttngs.settings_2d = settings_PIETOOLS_stripped_2D;
+    case 'veryheavy'
+        sttngs = settings_PIETOOLS_veryheavy;
+        sttngs.settings_2d = settings_PIETOOLS_veryheavy_2D;
+    otherwise
+        warning('Unknown settings parameter requested. Defaulting to light settings');
+        sttngs = settings_PIETOOLS_light;
+        sttngs.settings_2d = settings_PIETOOLS_light_2D;
 end
+
+% Set strictness of positivity of LF and negativity of its derivative.
+sttngs.eppos = 1e-4;      % Positivity of Lyapunov Function with respect to real-valued states
+sttngs.eppos2 = 1*1e-6;   % Positivity of Lyapunov Function with respect to spatially distributed states
+sttngs.epneg = derivative_strictness;    % Negativity of Derivative of Lyapunov Function in both ODE and PDE state
+
+% Set same settings for 2D case.
+sttngs.settings_2d.eppos = [1e-4; 1e-6; 1e-6; 1e-6];    % Positivity of LF wrt R x L2[s1] x L2[s2] x L2[s1,s2]
+sttngs.settings_2d.epneg = derivative_strictness;       % Negativity of Derivative of Lyapunov Function in both ODE and PDE state
+
 
 % Set the SOS solve settings (independent of dimension).
 sttngs.sos_opts.simplify = strcmp(simplify,'psimplify');    % Use psimplify in solving the SOS?
