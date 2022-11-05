2e840aa0d6a71c6fd26fbdd6bf1ef0db2fabf790
Declan <91631433+djagt@users.noreply.github.com>
2022-11-05 13:33:01 -0700

    Update Estimator Demo.
    The demo now constructs an estimator for a PDE with observation at the boundary.

--- PIETOOLS_2021b/PIETOOLS_demos/Hinf_optimal_estimator_DEMO.m
+++ PIETOOLS_2021b/PIETOOLS_demos/Hinf_optimal_estimator_DEMO.m
@@ -2,10 +2,11 @@
 % for a PIE, and how the estimated state can be simulated.
 %
 % We consider the system defined by:
-% PDE:      \dot{x}(t,s) = (d^2/ds^2) x(t,s) + 5 x(t,s) + w(t),    s in [0,1];
+% PDE:      \dot{x}(t,s) = (d^2/ds^2) x(t,s) + lam x(t,s) + w(t),   s in [0,1];
 % Outputs:       z(t)    = int_{0}^{1} x(t,s) ds + w(t);
-%                y(t)    = int_{0}^{1} x(t,s) ds;
-% BCs:                0  = x(t,0) = x(t,1);
+%                y(t)    = x(t,1);
+% BCs:                0  = x(t,0) = x_{s}(t,1);
+% (unstable for lam > pi^2/4 = 2.4674)
 %
 % Letting v:=(d^2/ds^2)x, We derive an equivalent PIE of the form:
 % [T \dot{v}](t,s) = [A v](t,s) + [B1 w](t,s);
@@ -46,14 +47,15 @@ echo on
 % % % Declare the PDE, and convert it to a PIE.
 % Declare the PDE using command line parser
 pvar s t
+lam = 4;
 PDE = sys();
 x = state('pde');   w = state('in');
 y = state('out');   z = state('out');
-eqs = [diff(x,t) - diff(x,s,2) - 5*x - w;
+eqs = [diff(x,t) - diff(x,s,2) - lam*x - w;
        z - int(x,s,[0,1]) - w;
-       y - int(x,s,[0,1]);
+       y - subs(x,s,1);
        subs(x,s,0);
-       subs(x,s,1)];
+       subs(diff(x,s),s,1)];
 PDE = addequation(PDE,eqs);
 PDE = setObserve(PDE,y);
 display_PDE(PDE);
@@ -154,8 +156,8 @@ PIE_CL = initialize(PIE_CL);
 
 % Declare initial conditions for the state components of the PIE
 syms st sx real
-uinput.ic.PDE = [20*(sx^2-sx);  % Actual initial PIE state value
-                 0];            % Estimated initial PIE state value
+uinput.ic.PDE = [-10*sx;    % Actual initial PIE state value
+                 0];        % Estimated initial PIE state value
  
 % Declare the value of the disturbance w(t)
 uinput.w = 2*sin(pi*st);
@@ -177,26 +179,39 @@ x_est = reshape(solution.timedep.pde(:,2,:),opts.N+1,[]);
 tval = solution.timedep.dtime;
 
 % Set options for the plot
-plot_indcs = floor(linspace(1,opts.tf/opts.dt,66)); 
+plot_indcs = floor(logspace(0,log(opts.tf/opts.dt)/log(10),40)); 
+%plot_indcs = floor(linspace(1,opts.tf/opts.dt,66)); 
 tplot = tval(plot_indcs);           % Only plot at select times
-colors = {'g','b','r','m','k'};     % Colors for the plot
-grid_idcs = [5,3,2];                % Only plot at a few grid points
+colors = {'b','g','m','r','k','c','r','y','o'};     % Colors for the plot
+grid_idcs = [1,5,7];                % Only plot at a few grid points
 
 % Plot evolution of actual and estimated
 fig1 = figure(1);
-hold on
 for j=grid_idcs
     s_pos = num2str(grid.phys(j));  % Position associated to grid index.
+    subplot(1,2,1)
+    hold on
     plot(tplot,x_act(j,plot_indcs),[colors{j},'-'],'LineWidth',2,'DisplayName',['$\mathbf{x}(s=',s_pos,')$']);
     plot(tplot,x_est(j,plot_indcs),[colors{j},'o--'],'LineWidth',1.5,'DisplayName',['$\mathbf{\hat{x}}(s=',s_pos,')$']);
+    hold off
+    
+    subplot(1,2,2)
+    hold on
+    loglog(tplot,(x_act(j,plot_indcs)-x_est(j,plot_indcs)),[colors{j},'o-'],'LineWidth',1.5,'DisplayName',['$\mathbf{e}(s=',s_pos,')$']);
+    hold off
 end
-hold off
 
 % Clean up the figure
-lgd = legend('Interpreter','latex');                lgd.FontSize=  10.5;
-xlabel('Time','FontSize',15,'Interpreter','latex');  ylabel('State Value','FontSize',15,'Interpreter','latex');
-title('Value of PDE state $x(t)$ and estimate $\hat{x}(t)$ at several grid points','Interpreter','latex','FontSize',16);
-fig1.Position = [700 600 800 450];
+ax1 = subplot(1,2,1);   ax1.XScale = 'log';     ax1.XTickLabels = {'0.001';'0.01';'0.1';'1'};
+lgd1 = legend('Interpreter','latex');                lgd1.FontSize = 10.5;
+lgd1.Location = 'northwest';
+xlabel('$t$','FontSize',15,'Interpreter','latex');  ylabel('$\mathbf{x}$','FontSize',15,'Interpreter','latex');
+title('PDE state value $\mathbf{x}(t)$ and estimate $\mathbf{\hat{x}}(t)$','Interpreter','latex','FontSize',15);
+ax2 = subplot(1,2,2);   ax2.XScale = 'log';     ax2.XTickLabels = {'0.001';'0.01';'0.1';'1'};
+lgd1 = legend('Interpreter','latex');                lgd2.FontSize = 10.5;
+xlabel('$t$','FontSize',15,'Interpreter','latex');    ylabel('$\mathbf{e}$','FontSize',15,'Interpreter','latex');
+title('Error $\mathbf{e}=\mathbf{\hat{x}}(t)-\mathbf{x}(t)$','Interpreter','latex','FontSize',15);
+fig1.Position = [700 600 1000 450];
 
 %%
 %%%%%%%%%%%%%%%%%% End Code Snippet %%%%%%%%%%%%%%%%%%
