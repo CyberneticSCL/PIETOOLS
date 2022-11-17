aabb4525906f87da8434d6d87e36ec4ceeabf905
Sachin <sachin.shivakumar@asu.edu>
2022-11-17 16:22:20 -0700

    Update l2gain_4.m

--- PIETOOLS_2021b/PIETOOLS_examples/Examples_Library/command_line_input_Examples/l2gain_4.m
+++ PIETOOLS_2021b/PIETOOLS_examples/Examples_Library/command_line_input_Examples/l2gain_4.m
@@ -8,7 +8,7 @@ x=state('pde',2); w = state('in'); z = state('out');
 pde = sys(); 
 R = 2.7;Cm = [1, 1.5; 5, 0.2];
 %% Define equations
-eq_PDE = diff(x,t)==Cm*x+(1/R)*diff(x,s,2)+s*w; % 	PDE: x_{t} = Cm*x + (1/R)*x_{ss}
+eq_PDE = diff(x,t)==Cm*x+(1/R)*diff(x,s,2)+s*[1;1]*w; % 	PDE: x_{t} = Cm*x + (1/R)*x_{ss}
 eq_BC = [subs(x,s,0)==0;subs(diff(x,s),s,1)==0]; %  BCs: x(s=0) = 0,     x_{s}(s=1) = 0 
 eq_out = [int(x,s,[0,1])];    %   Out: z(t) = int(x(t,s),s,0,1) 
 %% addequations to pde system; set control inputs/observed inputs, if any
