434f9d26f884e5640d204066cbd0f983ec37a472
Sachin <sachin.shivakumar@asu.edu>
2022-11-29 12:21:37 -0700

    Last header updates

--- PIETOOLS/settings/settings_PIETOOLS_stripped.m
+++ PIETOOLS/settings/settings_PIETOOLS_stripped.m
@@ -1,4 +1,31 @@
 function settings = settings_PIETOOLS_stripped()
+% Returns settings structure with degrees between 2 and 3 with lower
+% triangular structure in square root of positive definite PIs
+%
+% For support, contact M. Peet, Arizona State University at mpeet@asu.edu
+% or D. Jagt at djagt@asu.edu
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright (C)2022  M. Peet, S. Shivakumar, D. Jagt
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% If you modify this code, document all changes carefully and include date
+% authorship, and a brief description of modifications
 % DJ - 06/02/2021 - adjusted to store sosineq_on and clear auxilliary variables at the end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % internal variables:
