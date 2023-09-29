7eabc1cb8223a281352f3495a46e712dc23f39d2
Sachin <sachin.shivakumar@asu.edu>
2023-09-29 13:29:43 -0700

    Revert "edited dopvar concats"
    This reverts commit 282890f6f8eba24232387a8865b62c02ad41f671.

--- PIETOOLS/opvar/@dopvar/vertcat.m
+++ PIETOOLS/opvar/@dopvar/vertcat.m
@@ -39,156 +39,107 @@ function [Pcat] = vertcat(varargin)
 % DJ, 09/29/2021: Small adjustment to avoid error with dopvar-opvar concatenation
 % DJ, 12/30/2021: Adjusted to assure opvar with dopvar returns dopvar
 
+
 if nargin==1
     Pcat = varargin{1};
-    return
+else
+    a = varargin{1};
+    b = varargin{2};
+    
+    if isa(a,'dopvar') % correction to make components have consistent dimensions 8/27-ss
+        a.dim = a.dim;
+    end
+    if isa(b,'dopvar') % correction to make components have consistent dimensions 8/27-ss
+        b.dim = b.dim;
+    end
+    
+    dopvar Pcat;
+    if isa(a,'dopvar')
+        Pcat.I = a.I; Pcat.var1 = a.var1; Pcat.var2 = a.var2;
+    elseif isa(b,'dopvar')
+        Pcat.I = b.I; Pcat.var1 = b.var1; Pcat.var2 = b.var2;
+    elseif isa(a,'dopvar')&&isa(b,'dopvar')
+        if any(a.I~=b.I)||(a.var1~=b.var1)||(a.var2~=b.var2)
+            error('Operators being concatenated have different intervals or different independent variables');
+        end
+    end
+    
+    if ~isa(a,'dopvar')
+        if ~isa(b,'dopvar')
+            Pcat = [a;b];
+        else
+            bdim = b.dim;
+            if ~isa(a,'opvar') && size(a,2)~=sum(bdim(:,2)) % DJ 09/29/2021
+                error("Cannot concatentate vertically. A and B have different input dimensions");
+            end
+%             Pcat = b;
+            if bdim(2,2) ==0 && ~isa(a,'opvar')
+                Pcat.P = [a; b.P]; % a() is from R to R
+                Pcat.Q1 =[zeros(size(a,1),b.dim(2,2)); b.Q1];
+            elseif bdim(1,2)==0 && ~isa(a,'opvar')% a() is from L2 to L2, Note: a() cannot be a matrix and map L2 to R 
+                Pcat.Q2 = [zeros(size(a,1),b.dim(1,2)); b.Q2];
+                Pcat.R.R0 = [a; b.R.R0];
+                Pcat.R.R1 = [zeros(size(a)); b.R.R1];
+                Pcat.R.R2 = [zeros(size(a)); b.R.R2];
+            else %find if such a operation is valid is any useful scenario and implement it
+                if any(b.dim(:,2)~=a.dim(:,2))
+                    error("Cannot concatentate vertically. A and B have different input dimensions");
+                end
+%         Pcat = b;
+        fset = {'P', 'Q1', 'Q2'};
+        for i=fset
+            Pcat.(i{:}) = [a.(i{:}); b.(i{:})];
+        end
+        fset = {'R0','R1','R2'};
+        for i=fset
+            Pcat.R.(i{:}) = [a.R.(i{:}); b.R.(i{:})];
+        end
+            end
+        end
+    elseif ~isa(b,'dopvar')
+        adim = a.dim;
+        if ~isa(b,'opvar') && size(b,2)~=sum(adim(:,2)) % DJ 09/29/2021
+            error("Cannot concatentate vertically. A and B have different input dimensions");
+        end
+%         Pcat = a;
+        if adim(2,2) ==0&& ~isa(b,'opvar')
+            Pcat.P = [a.P; b]; % b() is from R to R
+            Pcat.Q1 = [a.Q1; zeros(size(b,1),a.dim(2,2))];
+        elseif adim(1,2)==0 && ~isa(b,'opvar')% b() is from L2 to L2, Note: b() cannot be L2 to R and not be opvar
+            Pcat.Q2 = [a.Q2; zeros(size(b,1),a.dim(1,2))];
+            Pcat.R.R0 = [a.R.R0; b];
+            Pcat.R.R1 = [a.R.R1; zeros(size(b))];
+            Pcat.R.R2 = [a.R.R2; zeros(size(b))];
+        else %find if such a operation is valid is any useful scenario and implement it
+            if any(b.dim(:,2)~=a.dim(:,2))
+                error("Cannot concatentate vertically. A and B have different input dimensions");
+            end
+%         Pcat = a;
+        fset = {'P', 'Q1', 'Q2'};
+        for i=fset
+            Pcat.(i{:}) = [a.(i{:}); b.(i{:})];
+        end
+        fset = {'R0','R1','R2'};
+        for i=fset
+            Pcat.R.(i{:}) = [a.R.(i{:}); b.R.(i{:})];
+        end
+        end
+    else
+        if any(b.dim(:,2)~=a.dim(:,2))
+            error("Cannot concatentate vertically. A and B have different input dimensions");
+        end
+%         Pcat = a;
+        fset = {'P', 'Q1', 'Q2'};
+        for i=fset
+            Pcat.(i{:}) = [a.(i{:}); b.(i{:})];
+        end
+        fset = {'R0','R1','R2'};
+        for i=fset
+            Pcat.R.(i{:}) = [a.R.(i{:}); b.R.(i{:})];
+        end
+    end
+    if nargin>2 % Continue concatenation if inputs are more than 2
+        Pcat = vertcat(Pcat, varargin{3:end});
+    end
 end
-
-% Extract the operators
-a = varargin{1};    b = varargin{2};
-
-% Error checking
-if (~isa(a,'dopvar')&&~isa(a,'opvar')) || (~isa(b,'dopvar')&&~isa(b,'opvar'))
-    error('Concatenation of opvar and non-opvar objects is ambiguous, and currently not supported')
 end
-if any(a.I~=b.I)|| ~strcmp(a.var1.varname,b.var1.varname) || ~strcmp(a.var2.varname,b.var2.varname)
-    error('Operators being concatenated have different intervals or different independent variables');
-end
-
-% Check that the output dimensions match
-a.dim = a.dim;  b.dim = b.dim;  % correction to make components have consistent dimensions 8/27-ss
-if any(a.dim(:,2)~=b.dim(:,2))
-    error('Cannot concatenate vertically: Input dimensions of opvar objects does not match')
-end
-
-% Avoid "ambiguous" concatenations
-if (a.dim(2,1)~=0 && b.dim(1,1)~=0)
-    % a has rows mapping to L2, but b has rows mapping to R.
-    % Concatenation would place those rows of a below those of b in the
-    % opvar, which we currently prohibit...
-    error('Proposed opvar concatenation is ambiguous, and currently prohibited')
-end
-
-% Finally, let's actually concatenate
-opvar Pcat;
-Pcat.I = a.I;   Pcat.var1 = a.var1;     Pcat.var2 = a.var2;
-fset = {'P', 'Q1', 'Q2'};
-for i=fset
-    Pcat.(i{:}) = [a.(i{:}); b.(i{:})];
-end
-fset = {'R0','R1','R2'};
-for i=fset
-    Pcat.R.(i{:}) = [a.R.(i{:}); b.R.(i{:})];
-end
-
-% For concatenation of more than two objects, just repeat
-if nargin>2 
-    Pcat = vertcat(Pcat, varargin{3:end});
-end
-
-end
-
-% old code
-% if nargin==1
-%     Pcat = varargin{1};
-% else
-%     a = varargin{1};
-%     b = varargin{2};
-%     
-%     if isa(a,'dopvar') % correction to make components have consistent dimensions 8/27-ss
-%         a.dim = a.dim;
-%     end
-%     if isa(b,'dopvar') % correction to make components have consistent dimensions 8/27-ss
-%         b.dim = b.dim;
-%     end
-%     
-%     dopvar Pcat;
-%     if isa(a,'dopvar')
-%         Pcat.I = a.I; Pcat.var1 = a.var1; Pcat.var2 = a.var2;
-%     elseif isa(b,'dopvar')
-%         Pcat.I = b.I; Pcat.var1 = b.var1; Pcat.var2 = b.var2;
-%     elseif isa(a,'dopvar')&&isa(b,'dopvar')
-%         if any(a.I~=b.I)||(a.var1~=b.var1)||(a.var2~=b.var2)
-%             error('Operators being concatenated have different intervals or different independent variables');
-%         end
-%     end
-%     
-%     if ~isa(a,'dopvar')
-%         if ~isa(b,'dopvar')
-%             Pcat = [a;b];
-%         else
-%             bdim = b.dim;
-%             if ~isa(a,'opvar') && size(a,2)~=sum(bdim(:,2)) % DJ 09/29/2021
-%                 error("Cannot concatentate vertically. A and B have different input dimensions");
-%             end
-% %             Pcat = b;
-%             if bdim(2,2) ==0 && ~isa(a,'opvar')
-%                 Pcat.P = [a; b.P]; % a() is from R to R
-%                 Pcat.Q1 =[zeros(size(a,1),b.dim(2,2)); b.Q1];
-%             elseif bdim(1,2)==0 && ~isa(a,'opvar')% a() is from L2 to L2, Note: a() cannot be a matrix and map L2 to R 
-%                 Pcat.Q2 = [zeros(size(a,1),b.dim(1,2)); b.Q2];
-%                 Pcat.R.R0 = [a; b.R.R0];
-%                 Pcat.R.R1 = [zeros(size(a)); b.R.R1];
-%                 Pcat.R.R2 = [zeros(size(a)); b.R.R2];
-%             else %find if such a operation is valid is any useful scenario and implement it
-%                 if any(b.dim(:,2)~=a.dim(:,2))
-%                     error("Cannot concatentate vertically. A and B have different input dimensions");
-%                 end
-% %         Pcat = b;
-%         fset = {'P', 'Q1', 'Q2'};
-%         for i=fset
-%             Pcat.(i{:}) = [a.(i{:}); b.(i{:})];
-%         end
-%         fset = {'R0','R1','R2'};
-%         for i=fset
-%             Pcat.R.(i{:}) = [a.R.(i{:}); b.R.(i{:})];
-%         end
-%             end
-%         end
-%     elseif ~isa(b,'dopvar')
-%         adim = a.dim;
-%         if ~isa(b,'opvar') && size(b,2)~=sum(adim(:,2)) % DJ 09/29/2021
-%             error("Cannot concatentate vertically. A and B have different input dimensions");
-%         end
-% %         Pcat = a;
-%         if adim(2,2) ==0&& ~isa(b,'opvar')
-%             Pcat.P = [a.P; b]; % b() is from R to R
-%             Pcat.Q1 = [a.Q1; zeros(size(b,1),a.dim(2,2))];
-%         elseif adim(1,2)==0 && ~isa(b,'opvar')% b() is from L2 to L2, Note: b() cannot be L2 to R and not be opvar
-%             Pcat.Q2 = [a.Q2; zeros(size(b,1),a.dim(1,2))];
-%             Pcat.R.R0 = [a.R.R0; b];
-%             Pcat.R.R1 = [a.R.R1; zeros(size(b))];
-%             Pcat.R.R2 = [a.R.R2; zeros(size(b))];
-%         else %find if such a operation is valid is any useful scenario and implement it
-%             if any(b.dim(:,2)~=a.dim(:,2))
-%                 error("Cannot concatentate vertically. A and B have different input dimensions");
-%             end
-% %         Pcat = a;
-%         fset = {'P', 'Q1', 'Q2'};
-%         for i=fset
-%             Pcat.(i{:}) = [a.(i{:}); b.(i{:})];
-%         end
-%         fset = {'R0','R1','R2'};
-%         for i=fset
-%             Pcat.R.(i{:}) = [a.R.(i{:}); b.R.(i{:})];
-%         end
-%         end
-%     else
-%         if any(b.dim(:,2)~=a.dim(:,2))
-%             error("Cannot concatentate vertically. A and B have different input dimensions");
-%         end
-% %         Pcat = a;
-%         fset = {'P', 'Q1', 'Q2'};
-%         for i=fset
-%             Pcat.(i{:}) = [a.(i{:}); b.(i{:})];
-%         end
-%         fset = {'R0','R1','R2'};
-%         for i=fset
-%             Pcat.R.(i{:}) = [a.R.(i{:}); b.R.(i{:})];
-%         end
-%     end
-%     if nargin>2 % Continue concatenation if inputs are more than 2
-%         Pcat = vertcat(Pcat, varargin{3:end});
-%     end
-% end
-% end
