866e3fe75774c38d7c3e2b3fabf22a03f5222d20
Declan <91631433+djagt@users.noreply.github.com>
2023-09-30 15:23:59 -0700

    Update opvar concatenations.
    When concatenating opvar (opvar2d, dopvar, dopvar2d) objects with non-opvar objects, there is some ambiguity regarding what spaces these non-opvar objects map to and from. The new version therefore prohibits concatenation of opvar and non-opvar objects. In addition, concatenation of any opvar objects for which the result would map to or from L2xR rather than RxL2 is now also prohibited, as such maps cannot be represented using the opvar structure.

--- PIETOOLS/opvar/@opvar/horzcat.m
+++ PIETOOLS/opvar/@opvar/horzcat.m
@@ -1,12 +1,15 @@
 function [Pcat] = horzcat(varargin) 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% [Pcat] = horzcat(varargin) takes n-inputs and concatentates them horizontally,
-% provided they satisfy the following criterias.
-% 1) Atleast one input is an opvar variable.
-% 2) If all the inputs are not opvar, then the operator maps from RxL2 to
-%    L2 or R to R. 
-% 3) Currently, it supports RxL2 to RxL2 concatenation only if ALL the inputs are
-%    opvar.
+% [Pcat] = horzcat(varargin) takes n inputs and concatentates them horizontally,
+% provided they satisfy the following criteria:
+% 1) All inputs must of type 'opvar';
+% 2) The output dimensions varargin{j}.dim(:,1) of all opvar objects must
+%       match;
+% 3) The spatial variables varargin{j}.var1 and varargin{j}.var2, as well 
+%       as the domain varargin{j}.I of all opvar objects must match;
+% 4) Concatenation should make sense within the context of the opvar class,
+%       that is, opvars take inputs from RxL2, never from L2xR. We cannot
+%       concatenate e.g. [A,B] for A:L2-->R and B:R-->R.
 % 
 % NOTES:
 % For support, contact M. Peet, Arizona State University at mpeet@asu.edu
@@ -41,95 +44,190 @@ function [Pcat] = horzcat(varargin)
 % SS - 8/27/19 added dimension correction to work with new opvar class that
 % has automatic dimension calculation
 % Adjusted so that dpvar with opvar returns dopvar, DJ 12/30/2021.
+% DJ - 09/30/23: Prohibit "ambiguous" concatenations.
 
-
+% Deal with single input case
 if nargin==1
     Pcat = varargin{1};
-else
-    a = varargin{1};
-    b = varargin{2};
-    
-    opvar Pcat;
-    if isa(a,'opvar')
-        Pcat.I = a.I; Pcat.var1 = a.var1; Pcat.var2 = a.var2;
-    elseif isa(b,'opvar')
-        Pcat.I = b.I; Pcat.var1 = b.var1; Pcat.var2 = b.var2;
-    elseif isa(a,'opvar')&&isa(b,'opvar')
-        if any(a.I~=b.I)||(a.var1~=b.var1)||(a.var2~=b.var2)
-            error('Operators being concatenated have different intervals or different independent variables');
-        end
-    end
-    if isa(a,'opvar') % correction to make components have consistent dimensions 8/27-ss
-        a.dim = a.dim;
-    end
-    if isa(b,'opvar') % correction to make components have consistent dimensions 8/27-ss
-        b.dim = b.dim;
-    end
-    
-    if isa(a,'dpvar')  % DJ, 12/30/2021
-        b = opvar2dopvar(b);
-        Pcat = horzcat(a,b);
-    elseif isa(b,'dpvar')
-        a = opvar2dopvar(a);
-        Pcat = horzcat(a,b);
-    elseif ~isa(a,'opvar')
-        if ~isa(b,'opvar') % both are not opvar variables
-            if size(a,1)~=size(b,1) 
-                error('Cannot concatentate horizontally. A and B have different output dimensions');
-            end
-            Pcat = [a b];
-        elseif b.dim(1,1)==0 % a() is from R to L2, Note: L2 to L2 needs to be an opvar
-            if size(a,1)~=b.dim(2,1) 
-                error('Cannot concatentate horizontally. A and B have different output dimensions');
-            end 
-%             Pcat = b;
-            Pcat.Q2 = [a b.Q2];
-            Pcat.P = [zeros(b.dim(1,1),size(a,2)) b.P];
-        elseif b.dim(2,1)==0 % a() is from R to R, Note: L2 to R needs to be an opvar
-            if size(a,1)~=b.dim(1,1) 
-                error('Cannot concatentate horizontally. A and B have different output dimensions');
-            end
-%             Pcat = b;
-            Pcat.P = [a b.P];
-            Pcat.Q2 = [zeros(b.dim(2,1),size(a,2)) b.Q2];
-        else %find if such an operation is valid is any useful scenario and implement it
-            error('Cannot concatenate horizontally. This feature is not yet supported.');
-        end
-    elseif ~isa(b,'opvar')
-        if a.dim(1,1)==0 % b() is from R to L2, Note: L2 to L2 needs to be an opvar
-            if size(b,1)~=a.dim(2,1) 
-                error('Cannot concatentate horizontally. A and B have different output dimensions');
-            end
-%             Pcat = a;
-            Pcat.Q2 = [a.Q2 b];
-            Pcat.P = [a.P zeros(a.dim(1,1),size(b,2))];
-        elseif a.dim(2,1)==0 % b() is from R to R, Note: L2 to R needs to be an opvar
-            if size(b,1)~=a.dim(1,1) 
-                error('Cannot concatentate horizontally. A and B have different output dimensions');
-            end
-%             Pcat = a;
-            Pcat.P = [a.P b];
-            Pcat.Q2 = [a.Q2 zeros(a.dim(2,1),size(b,2))];
-        else %find if such a operation is valid is any useful scenario and implement it
-            error('Cannot concatenate horizontally.This feature is not yet supported.');
-        end
-    else
-        if any(b.dim(:,1)~=a.dim(:,1))
-            error('Cannot concatentate horizontally. A and B have different output dimensions');
-        end
-%         Pcat = a;
-        fset = {'P', 'Q1', 'Q2'};
+    return
+end
 
-        for i=fset
-            Pcat.(i{:}) = [a.(i{:}) b.(i{:})];
-        end
-        fset = {'R0','R1','R2'};
-        for i=fset
-            Pcat.R.(i{:}) = [a.R.(i{:}) b.R.(i{:})];
-        end
-    end
-    if nargin>2 % check if there are more than 2 objects that need to stacked
-        Pcat = horzcat(Pcat, varargin{3:end});
-    end
+% Extract the operators
+a = varargin{1};    b = varargin{2};
+
+% Currently support only opvar-opvar concatenation
+if ~isa(a,'opvar') || ~isa(b,'opvar')
+    error('Concatenation of opvar and non-opvar objects is ambiguous, and currently not supported')
 end
+% Check that domain and variables match
+if any(a.I~=b.I)|| ~strcmp(a.var1.varname,b.var1.varname) || ~strcmp(a.var2.varname,b.var2.varname)
+    error('Operators being concatenated have different intervals or different independent variables');
 end
+% Check that the output dimensions match
+a.dim = a.dim;  b.dim = b.dim;  % correction to make components have consistent dimensions 8/27-ss
+if any(a.dim(:,1)~=b.dim(:,1))
+    error('Cannot concatenate horizontally: Output dimensions of opvar objects do not match')
+end
+
+% Avoid "ambiguous" concatenations
+if (a.dim(2,2)~=0 && b.dim(1,2)~=0)
+    % a has columns mapping from L2, but b has columns mapping from R.
+    % Concatenation would place those columns of a to the right of
+    % those columns of b in the opvar, which we currently prohibit...
+    error('Proposed opvar concatenation is ambiguous, and currently prohibited')
+end
+
+% Finally, let's actually concatenate
+opvar Pcat;
+Pcat.I = a.I;   Pcat.var1 = a.var1;     Pcat.var2 = a.var2;
+fset = {'P', 'Q1', 'Q2'};
+for i=fset
+    Pcat.(i{:}) = [a.(i{:}) b.(i{:})];
+end
+fset = {'R0','R1','R2'};
+for i=fset
+    Pcat.R.(i{:}) = [a.R.(i{:}) b.R.(i{:})];
+end
+
+% For concatenation of more than two objects, just repeat
+if nargin>2 
+    Pcat = horzcat(Pcat, varargin{3:end});
+end
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%  Old version, that allows "ambiguous" concatenation   %%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% [Pcat] = horzcat(varargin) takes n-inputs and concatentates them horizontally,
+% provided they satisfy the following criteria:
+% 1) Atleast one input is an opvar variable.
+% 2) If all the inputs are not opvar, then the operator maps from RxL2 to
+%    L2 or R to R. 
+% 3) Currently, it supports RxL2 to RxL2 concatenation only if ALL the inputs are
+%    opvar.
+% 
+% NOTES:
+% For support, contact M. Peet, Arizona State University at mpeet@asu.edu
+% or S. Shivakumar at sshivak8@asu.edu
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% if nargin==1
+%     Pcat = varargin{1};
+% else
+%     a = varargin{1};
+%     b = varargin{2};
+%     
+%     opvar Pcat;
+%     if isa(a,'opvar')
+%         Pcat.I = a.I; Pcat.var1 = a.var1; Pcat.var2 = a.var2;
+%     elseif isa(b,'opvar')
+%         Pcat.I = b.I; Pcat.var1 = b.var1; Pcat.var2 = b.var2;
+%     elseif isa(a,'opvar')&&isa(b,'opvar')
+%         if any(a.I~=b.I)||(a.var1~=b.var1)||(a.var2~=b.var2)
+%             error('Operators being concatenated have different intervals or different independent variables');
+%         end
+%     end
+%     if isa(a,'opvar') % correction to make components have consistent dimensions 8/27-ss
+%         a.dim = a.dim;
+%     end
+%     if isa(b,'opvar') % correction to make components have consistent dimensions 8/27-ss
+%         b.dim = b.dim;
+%     end
+%     
+%     if isa(a,'dpvar')  % DJ, 12/30/2021
+%         b = opvar2dopvar(b);
+%         Pcat = horzcat(a,b);
+%     elseif isa(b,'dpvar')
+%         a = opvar2dopvar(a);
+%         Pcat = horzcat(a,b);
+%     elseif ~isa(a,'opvar')
+%         if ~isa(b,'opvar') % both are not opvar variables
+%             if size(a,1)~=size(b,1) 
+%                 error('Cannot concatentate horizontally. A and B have different output dimensions');
+%             end
+%             Pcat = [a b];
+%         elseif b.dim(1,1)==0
+%             % b:RxL2-->L2, assume a:R-->L2
+%             % NOTE: a:L2-->L2 must be specified as opvar
+%             if size(a,1)~=b.dim(2,1) 
+%                 error('Cannot concatentate horizontally. A and B have different output dimensions');
+%             end 
+%             %Pcat.P = [zeros(b.dim(1,1),size(a,2)), b.P];
+%             Pcat.Q2 = [a,b.Q2];
+%             Pcat.R.R0 = b.R.R0;
+%             Pcat.R.R1 = b.R.R1;
+%             Pcat.R.R2 = b.R.R2;
+% %             Pcat.Q2 = b.Q2;
+% %             Pcat.R.R0 = [a, b.R.R0];
+% %             Pcat.R.R1 = [zeros(b.dim(2,1),size(a,2)), b.R.R1];
+% %             Pcat.R.R2 = [zeros(b.dim(2,1),size(a,2)), b.R.R2];
+%         elseif b.dim(2,1)==0 
+%             % b:RxL2-->R, assume a:R-->R
+%             if size(a,1)~=b.dim(1,1) 
+%                 error('Cannot concatentate horizontally. A and B have different output dimensions');
+%             end
+%             if isa(a,'polynomial') || isa(b,'dpvar')
+%                 try a=double(a);
+%                 catch
+%                     error('Convert arguments to opvar for this concatenation')
+%                 end
+%             end
+%             Pcat.P = [a b.P];
+%             Pcat.Q1 = b.Q1;
+%             %Pcat.Q2 = [zeros(b.dim(2,1),size(a,2)) b.Q2];
+%         else %find if such an operation is valid is any useful scenario and implement it
+%             error('Cannot concatenate horizontally. This feature is not yet supported.');
+%         end
+%     elseif ~isa(b,'opvar')
+%         if a.dim(1,1)==0
+%             % a:RxL2-->L2, assume b:R-->L2
+%             % NOTE: b:L2-->L2 must be specified as opvar
+%             if size(b,1)~=a.dim(2,1) 
+%                 error('Cannot concatentate horizontally. A and B have different output dimensions');
+%             end
+%             %Pcat.P = [a.P zeros(a.dim(1,1),size(b,2))];
+%             Pcat.Q2 = [a.Q2, b];
+%             Pcat.R.R0 = a.R.R0;
+%             Pcat.R.R1 = a.R.R1;
+%             Pcat.R.R2 = a.R.R2;
+% %             Pcat.Q2 = a.Q2;
+% %             Pcat.R.R0 = [a.R.R0, b];
+% %             Pcat.R.R1 = [a.R.R1, zeros(a.dim(2,1),size(b,2))];
+% %             Pcat.R.R2 = [a.R.R2, zeros(a.dim(2,1),size(b,2))];
+%         elseif a.dim(2,1)==0
+%             % a:RxL2-->R, assume b:R-->R
+%             if size(b,1)~=a.dim(1,1) 
+%                 error('Cannot concatentate horizontally. A and B have different output dimensions');
+%             end
+%             if isa(a,'polynomial') || isa(b,'dpvar')
+%                 try a=double(a);
+%                 catch
+%                     error('Convert arguments to opvar for this concatenation')
+%                 end
+%             end
+%             Pcat.P = [a.P b];
+%             Pcat.Q1 = a.Q1;
+%             %Pcat.Q2 = [a.Q2 zeros(a.dim(2,1),size(b,2))];
+%         else %find if such a operation is valid is any useful scenario and implement it
+%             error('Cannot concatenate horizontally.This feature is not yet supported.');
+%         end
+%     else
+%         if any(b.dim(:,1)~=a.dim(:,1))
+%             error('Cannot concatentate horizontally. A and B have different output dimensions');
+%         end
+%         fset = {'P', 'Q1', 'Q2'};
+%         for i=fset
+%             Pcat.(i{:}) = [a.(i{:}) b.(i{:})];
+%         end
+%         fset = {'R0','R1','R2'};
+%         for i=fset
+%             Pcat.R.(i{:}) = [a.R.(i{:}) b.R.(i{:})];
+%         end
+%     end
+%     if nargin>2 % check if there are more than 2 objects that need to stacked
+%         Pcat = horzcat(Pcat, varargin{3:end});
+%     end
+% end
--- PIETOOLS/opvar/@opvar/vertcat.m
+++ PIETOOLS/opvar/@opvar/vertcat.m
@@ -1,12 +1,15 @@
 function [Pcat] = vertcat(varargin)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% [Pcat] = vertcat(varargin) takes n-inputs and concatentates them vertically,
-% provided they satisfy the following criterias.
-% 1) Atleast one input is an opvar variable.
-% 2) If all the inputs are not opvar, then the operator maps from R to
-% RxL2 or L2 to L2. 
-% 3) Currently, it supports RxL2 to RxL2 concatenation only if ALL the inputs are
-% opvar.
+% [Pcat] = vertcat(varargin) takes n inputs and concatentates them vertically,
+% provided they satisfy the following criteria:
+% 1) All inputs must of type 'opvar';
+% 2) The input dimensions varargin{j}.dim(:,2) of all opvar objects must
+%       match;
+% 3) The spatial variables varargin{j}.var1 and varargin{j}.var2, as well 
+%       as the domain varargin{j}.I of all opvar objects must match;
+% 4) Concatenation should make sense within the context of the opvar class,
+%       that is, opvars always mapt to from RxL2, never to L2xR. We cannot
+%       concatenate e.g. [A;B] for A:R-->L2 and B:R-->R.
 %
 % NOTES:
 % For support, contact M. Peet, Arizona State University at mpeet@asu.edu
@@ -41,94 +44,191 @@ function [Pcat] = vertcat(varargin)
 % SS - 8/27/19 added dimension correction to work with new opvar class that
 % has automatic dimension calculation
 % Adjusted so that dpvar with opvar returns dopvar, DJ 12/30/2021.
+% DJ - 09/30/23: Prohibit "ambiguous" concatenations.
 
-
+% Deal with single input case
 if nargin==1
     Pcat = varargin{1};
-else
-    a = varargin{1};
-    b = varargin{2};
-    
-    opvar Pcat;
-    if isa(a,'opvar')
-        Pcat.I = a.I; Pcat.var1 = a.var1; Pcat.var2 = a.var2;
-    elseif isa(b,'opvar')
-        Pcat.I = b.I; Pcat.var1 = b.var1; Pcat.var2 = b.var2;
-    elseif isa(a,'opvar')&&isa(b,'opvar')
-        if any(a.I~=b.I)||(a.var1~=b.var1)||(a.var2~=b.var2)
-            error('Operators being concatenated have different intervals or different independent variables');
-        end
-    end
-    if isa(a,'opvar') % correction to make components have consistent dimensions 8/27-ss
-        a.dim = a.dim;
-    end
-    if isa(b,'opvar') % correction to make components have consistent dimensions 8/27-ss
-        b.dim = b.dim;
-    end
-    
-    if isa(a,'dpvar')  % DJ, 12/30/2021
-        b = opvar2dopvar(b);
-        Pcat = vertcat(a,b);
-    elseif isa(b,'dpvar')
-        a = opvar2dopvar(a);
-        Pcat = vertcat(a,b);
-    elseif ~isa(a,'opvar') 
-        if ~isa(b,'opvar')
-            if size(a,2)~=size(b,2)
-                error('Cannot concatentate vertically. A and B have different input dimensions');
-            end
-            Pcat = [a;b];
-        else
-            bdim = b.dim;
-            if size(a,2)~=sum(bdim(:,2))
-                error("Cannot concatentate vertically. A and B have different input dimensions");
-            end
-%             Pcat = b;
-            if bdim(2,2) ==0
-                Pcat.P = [a; b.P]; % a() is from R to R
-                Pcat.Q1 =[zeros(size(a,1),b.dim(2,2)); b.Q1];
-            elseif bdim(1,2)==0 % a() is from L2 to L2, Note: a() cannot be a matrix and map L2 to R 
-                Pcat.Q2 = [zeros(size(a,1),b.dim(1,2)); b.Q2];
-                Pcat.R.R0 = [a; b.R.R0];
-                Pcat.R.R1 = [zeros(size(a)); b.R.R1];
-                Pcat.R.R2 = [zeros(size(a)); b.R.R2];
-            else %find if such a operation is valid is any useful scenario and implement it
-                error('Cannot concatenate vertically. This feature is not yet supported.');
-            end
-        end
-    elseif ~isa(b,'opvar') 
-        adim = a.dim;
-        if size(b,2)~=sum(adim(:,2))
-            error("Cannot concatentate vertically. A and B have different input dimensions");
-        end
-%         Pcat = a;
-        if adim(2,2) ==0
-            Pcat.P = [a.P; b]; % b() is from R to R
-            Pcat.Q1 = [a.Q1; zeros(size(b,1),a.dim(2,2))];
-        elseif adim(1,2)==0 % b() is from L2 to L2, Note: b() cannot be L2 to R and not be opvar
-            Pcat.Q2 = [a.Q2; zeros(size(b,1),a.dim(1,2))];
-            Pcat.R.R0 = [a.R.R0; b];
-            Pcat.R.R1 = [a.R.R1; zeros(size(b))];
-            Pcat.R.R2 = [a.R.R2; zeros(size(b))];
-        else %find if such a operation is valid is any useful scenario and implement it
-            error('Cannot concatenate vertically. This feature is not yet supported.');
-        end
-    else
-        if any(b.dim(:,2)~=a.dim(:,2))
-            error("Cannot concatentate vertically. A and B have different input dimensions");
-        end
-%         Pcat = a;
-        fset = {'P', 'Q1', 'Q2'};
-        for i=fset
-            Pcat.(i{:}) = [a.(i{:}); b.(i{:})];
-        end
-        fset = {'R0','R1','R2'};
-        for i=fset
-            Pcat.R.(i{:}) = [a.R.(i{:}); b.R.(i{:})];
-        end
-    end
-    if nargin>2 % Continue concatenation if inputs are more than 2
-        Pcat = vertcat(Pcat, varargin{3:end});
-    end
+    return
 end
+
+% Extract the operators
+a = varargin{1};    b = varargin{2};
+
+% Currently support only opvar-opvar concatenation
+if ~isa(a,'opvar') || ~isa(b,'opvar')
+    error('Concatenation of opvar and non-opvar objects is ambiguous, and currently not supported')
 end
+% Check that domain and variables match
+if any(a.I~=b.I)|| ~strcmp(a.var1.varname,b.var1.varname) || ~strcmp(a.var2.varname,b.var2.varname)
+    error('Operators being concatenated have different intervals or different independent variables');
+end
+% Check that the output dimensions match
+a.dim = a.dim;  b.dim = b.dim;  % correction to make components have consistent dimensions 8/27-ss
+if any(a.dim(:,2)~=b.dim(:,2))
+    error('Cannot concatenate vertically: Input dimensions of opvar objects do not match')
+end
+
+% Avoid "ambiguous" concatenations
+if (a.dim(2,1)~=0 && b.dim(1,1)~=0)
+    % a has rows mapping to L2, but b has rows mapping to R.
+    % Concatenation would place those rows of a below those of b in the
+    % opvar, which we currently prohibit...
+    error('Proposed opvar concatenation is ambiguous, and currently prohibited')
+end
+
+% Finally, let's actually concatenate
+opvar Pcat;
+Pcat.I = a.I;   Pcat.var1 = a.var1;     Pcat.var2 = a.var2;
+fset = {'P', 'Q1', 'Q2'};
+for i=fset
+    Pcat.(i{:}) = [a.(i{:}); b.(i{:})];
+end
+fset = {'R0','R1','R2'};
+for i=fset
+    Pcat.R.(i{:}) = [a.R.(i{:}); b.R.(i{:})];
+end
+
+% For concatenation of more than two objects, just repeat
+if nargin>2 
+    Pcat = vertcat(Pcat, varargin{3:end});
+end
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%  Old version, that allows "ambiguous" concatenation   %%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% [Pcat] = vertcat(varargin) takes n-inputs and concatentates them vertically,
+% provided they satisfy the following criteria:
+% 1) Atleast one input is an opvar variable.
+% 2) If all the inputs are not opvar, then the operator maps from R to
+% RxL2 or L2 to L2. 
+% 3) Currently, it supports RxL2 to RxL2 concatenation only if ALL the inputs are
+% opvar.
+%
+% NOTES:
+% For support, contact M. Peet, Arizona State University at mpeet@asu.edu
+% or S. Shivakumar at sshivak8@asu.edu
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% if nargin==1
+%     Pcat = varargin{1};
+% else
+%     a = varargin{1};
+%     b = varargin{2};
+%     
+%     opvar Pcat;
+%     if isa(a,'opvar')
+%         Pcat.I = a.I; Pcat.var1 = a.var1; Pcat.var2 = a.var2;
+%     elseif isa(b,'opvar')
+%         Pcat.I = b.I; Pcat.var1 = b.var1; Pcat.var2 = b.var2;
+%     elseif isa(a,'opvar')&&isa(b,'opvar')
+%         if any(a.I~=b.I)||(a.var1~=b.var1)||(a.var2~=b.var2)
+%             error('Operators being concatenated have different intervals or different independent variables');
+%         end
+%     end
+%     if isa(a,'opvar') % correction to make components have consistent dimensions 8/27-ss
+%         a.dim = a.dim;
+%     end
+%     if isa(b,'opvar') % correction to make components have consistent dimensions 8/27-ss
+%         b.dim = b.dim;
+%     end
+%     
+%     if isa(a,'dpvar')  % DJ, 12/30/2021
+%         b = opvar2dopvar(b);
+%         Pcat = vertcat(a,b);
+%     elseif isa(b,'dpvar')
+%         a = opvar2dopvar(a);
+%         Pcat = vertcat(a,b);
+%     elseif ~isa(a,'opvar') 
+%         if ~isa(b,'opvar')
+%             if size(a,2)~=size(b,2)
+%                 error('Cannot concatentate vertically. A and B have different input dimensions');
+%             end
+%             Pcat = [a;b];
+%         else
+%             bdim = b.dim;
+%             if size(a,2)~=sum(bdim(:,2))
+%                 error("Cannot concatentate vertically. A and B have different input dimensions");
+%             end
+% %             Pcat = b;
+%             if bdim(2,2) ==0
+%                 % b:R-->RxL2, assume a:R-->R
+%                 if isa(a,'polynomial') || isa(b,'dpvar')
+%                     try a=double(a);
+%                     catch
+%                         error('Convert arguments to opvar for this concatenation')
+%                     end
+%                 end
+%                 Pcat.P = [a; b.P]; % a() is from R to R
+%                 Pcat.Q1 = [zeros(size(a,1),b.dim(2,2)); b.Q1];
+%                 Pcat.Q2 = b.Q2;
+%             elseif bdim(1,2)==0 
+%                 % b:L2-->RxL2, assume a:L2-->R
+%                 % NOTE: a:L2-->L2 must be specified as opvar
+%                 %Pcat.Q2 = [zeros(size(a,1),bdim(1,2)); b.Q2];
+%                 Pcat.Q1 = [a; b.Q1];
+%                 Pcat.R.R0 = b.R.R0;
+%                 Pcat.R.R1 = b.R.R1;
+%                 Pcat.R.R2 = b.R.R2;
+% %                 Pcat.Q1 = b.Q1;
+% %                 Pcat.R.R0 = [a; b.R.R0];
+% %                 Pcat.R.R1 = [zeros(size(a)); b.R.R1];
+% %                 Pcat.R.R2 = [zeros(size(a)); b.R.R2];
+%             else %find if such a operation is valid is any useful scenario and implement it
+%                 error('Cannot concatenate vertically. This feature is not yet supported.');
+%             end
+%         end
+%     elseif ~isa(b,'opvar') 
+%         adim = a.dim;
+%         if size(b,2)~=sum(adim(:,2))
+%             error("Cannot concatentate vertically. A and B have different input dimensions");
+%         end
+% %         Pcat = a;
+%         if adim(2,2) ==0
+%             % a:R-->RxL2, assume b:R-->R
+%             if isa(b,'polynomial') || isa(b,'dpvar')
+%                 try b=double(b);
+%                 catch
+%                     error('Convert arguments to opvar for this concatenation')
+%                 end
+%             end
+%             Pcat.P = [a.P; b]; % b() is from R to R
+%             Pcat.Q1 = [a.Q1; zeros(size(b,1),a.dim(2,2))];
+%             Pcat.Q2 = a.Q2;
+%         elseif adim(1,2)==0 
+%             % a:L2-->RxL2, assume b:L2-->R
+%             % NOTE: b:L2-->L2 must be specified as opvar
+%             %Pcat.Q2 = [a.Q2; zeros(size(b,1),adim(1,2))];
+%             Pcat.Q1 = [a.Q1; b];
+%             Pcat.R.R0 = a.R.R0;
+%             Pcat.R.R1 = a.R.R1;
+%             Pcat.R.R2 = a.R.R2;
+% %             Pcat.Q1 = a.Q1;
+% %             Pcat.R.R0 = [a.R.R0; b];
+% %             Pcat.R.R1 = [a.R.R1; zeros(size(b))];
+% %             Pcat.R.R2 = [a.R.R2; zeros(size(b))];
+%         else %find if such a operation is valid is any useful scenario and implement it
+%             error('Cannot concatenate vertically. This feature is not yet supported.');
+%         end
+%     else
+%         if any(b.dim(:,2)~=a.dim(:,2))
+%             error("Cannot concatentate vertically. A and B have different input dimensions");
+%         end
+% %         Pcat = a;
+%         fset = {'P', 'Q1', 'Q2'};
+%         for i=fset
+%             Pcat.(i{:}) = [a.(i{:}); b.(i{:})];
+%         end
+%         fset = {'R0','R1','R2'};
+%         for i=fset
+%             Pcat.R.(i{:}) = [a.R.(i{:}); b.R.(i{:})];
+%         end
+%     end
+%     if nargin>2 % Continue concatenation if inputs are more than 2
+%         Pcat = vertcat(Pcat, varargin{3:end});
+%     end
+% end
