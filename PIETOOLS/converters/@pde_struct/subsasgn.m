70887dbd7f168faaad29313973b943e03ddad7e6
Declan <91631433+djagt@users.noreply.github.com>
2024-06-23 16:40:19 -0700

    Update subsref and subsasgn.
    The updated subsref function allows term j in equation i of a PDE `PDE` to be retrieved as `PDE(i,j)`. Ideally, subsasgn would be updated so that the term (i,j) could be replaced as `PDE(i,j)=new_term`, but this has not happened. However, 'subsasgn' has been updated to allow the size, variables, and domain of a PDE variable (state, input, output) to be set as e.g. `x.size=2`, `x.vars=[ss1;ss2]`, and `x.dom=[0,1;1,2]`. The function 'subsref' has been updated to allow these field to be retrieved as well.

--- PIETOOLS/converters/@pde_struct/subsasgn.m
+++ PIETOOLS/converters/@pde_struct/subsasgn.m
@@ -54,7 +54,72 @@ function PDE = subsasgn(PDE,prop,val)
 if strcmp(prop(1).type,'()') || strcmp(prop(1).type,'{}')
     error(['''()'' and ''{}'' type subsasgn are currently not supported for "pde_struct" class objects.'])
 end
-PDE = builtin('subsasgn',PDE,prop,val);
+
+% If the PDE structure corresponds to a single variable (state, input, or
+% output), allow the variables, domain, and size to be set.
+[is_var,obj] = is_pde_var(PDE);
+if is_var && strcmp(prop(1).type,'.') && ismember(prop(1).subs,{'size';'vars';'dom'})
+    % Allow the size of the object to be set.
+    if strcmp(prop(1).subs,'size')
+        if ~isnumeric(val) || numel(val)~=1 || val<=0 || round(val)~=val
+            error("The desired size of the PDE variable should be specified as a scalar positive integer.")
+        else
+            PDE.(obj){1}.size = val;
+            PDE.([obj,'_tab'])(1,2) = val;
+        end
+    elseif strcmp(prop(1).subs,'vars')
+    % Allow the spatial variables on which the object depends to be set.
+        if ischar(val) && size(val,1)==1
+            % Convert variable name to polynomial.
+            vars = polynomial({val});
+        elseif iscellstr(val)
+            % Convert variable names to polynomials.
+            vars = polynomial(val);
+        elseif isempty(val)
+            % Set empty list of variables.
+            vars = polynomial(zeros(0,1));
+        elseif ~isempty(val) && ~ispvar(val)
+            error('Spatial variables for the desired object should be specified as an object of type ''polynomial'' or as a cell of variable names.' )
+        else
+            vars = val;
+        end
+        if ~isempty(val) && ~any(size(vars)==1)
+            error('Spatial variables for the desired object should be specified as array of size nx1.')
+        else
+            vars = vars(:);
+        end
+        PDE.(obj){1}.vars = vars;
+    elseif strcmp(prop(1).subs,'dom')
+    % Allow the domain of the variables to be set.
+        % If variables are specified, the number of domains should match
+        % the number of variables.
+        if isfield(PDE.(obj){1},'vars')
+            nvars = size(PDE.(obj){1}.vars,1);
+        else
+            nvars = nan;
+        end
+        dom = val;
+        if ~isa(dom,'double')
+            error('The domain of the spatial variables should be specified as nx2 array for n variables.')
+        elseif size(dom,2)~=2
+            error('The domain of the spatial variables should be specified as nx2 array for n variables.')
+        elseif any(dom(:,1)>=dom(:,2))
+            error('The specified domain makes no sense: lower boundary exceeds upper boundary!')
+        elseif ~isnan(nvars)
+            if size(dom,1)==1
+                % Assume same domain for all vars
+                dom = repmat(dom,nvars(1),1);
+            elseif size(dom,1)~=nvars
+                error('The number of specified domains should match the number of specified variables.')
+            end
+        end
+        PDE.(obj){1}.dom = dom;
+    end
+    return
+else
+    PDE = builtin('subsasgn',PDE,prop,val);
+end
+
 
 % If any property of the PDE has been adjusted, the PDE is no longer
 % initialized (in case it was before).
--- PIETOOLS/converters/@pde_struct/subsref.m
+++ PIETOOLS/converters/@pde_struct/subsref.m
@@ -51,13 +51,172 @@ function val = subsref(PDE,prop)
 % Initial coding DJ - 11/29/2022
 %
 
+% % % If the object is a single PDE variable, we have a separate case for
+% % % extracting information.
+[is_var,obj] = is_pde_var(PDE);
+
 % At this point, we just use the built-in subsasgn function. Additional
 % features such as '()' type subsasgn may be introduced in later updates.
-if strcmp(prop(1).type,'()') || strcmp(prop(1).type,'{}')
-    error(['''()'' and ''{}'' type subsref are currently not supported for "pie_struct" class objects.'])
-else
-    % Allow coefficients to be retrieved by calling PDE.C{i,j}
-    if strcmp(prop(1).type,'.') && strcmpi(prop(1).subs,'C')
+if strcmp(prop(1).type,'{}')
+    error(['''{}'' type subsref is currently not supported for "pde_struct" class objects.'])
+elseif strcmp(prop(1).type,'()')
+    % % Allow terms to be retrieved by calling PDE(i,j);
+    if is_var
+        PDE = var2term(PDE);
+    end
+    % % Process the inputs
+    sz_PDE = size(PDE);
+    if numel(prop(1).subs)==1 && sz_PDE(1)==1
+        % For single input case, if there is only one equation, 
+        % extract the desired term.
+        prop(1).subs = {1,prop(1).subs{1}};
+    elseif numel(prop(1).subs)==1
+        % In single input case for multiple equations, 
+        % assume full equations are desired.
+        prop(1).subs = {prop(1).subs{1},':'};
+    elseif numel(prop(1).subs)>2
+        error("Subsref is only supported with two inputs: equation number and term number.")
+    end
+    % Extract equation and term numbers
+    eq_idcs = prop(1).subs{1};       term_idcs = prop(1).subs{2};
+    if ischar(eq_idcs) && strcmp(eq_idcs,':')
+        % Assume all equations are desired.
+        eq_idcs = 1:sz_PDE(1);
+    elseif ~isnumeric(eq_idcs) || length(eq_idcs)~=numel(eq_idcs)
+        error("Equation indices should be specified as 1xn array of nonnegative integers.")
+    elseif isnumeric(eq_idcs)
+        if max(eq_idcs)>sz_PDE(1)
+            error("Equation indices cannot exceed the number of equations in the PDE!")
+        elseif any(eq_idcs<=0) || any(round(eq_idcs)~=eq_idcs)
+            error("Equation indices should be specified as 1xn array of nonnegative integers.")
+        end
+    end
+    if ischar(term_idcs) && strcmp(term_idcs,':')
+        % Assume all equations are desired.
+        term_idcs = 1:sz_PDE(2);
+    elseif ~isnumeric(term_idcs) || length(term_idcs)~=numel(term_idcs)
+        error("Equation indices should be specified as 1xn array of integers.")
+    elseif isnumeric(eq_idcs)
+        if max(term_idcs)>sz_PDE(2)
+            error("Term indices cannot exceed the maximal number of terms that appear in the equations!")
+        end
+    end
+
+    % % Build the output
+    val = PDE;
+    if ~isempty(PDE.free)
+        % % The input PDE describes only free terms
+        % % --> just return the desired terms
+        free_tmp = PDE.free;
+        for ii=1:numel(free_tmp)
+            if isfield(free_tmp{ii},'term') && ~isempty(free_tmp{ii}.term)
+                % Retain only terms specified by term_idcs.
+                term_idcs_ii = term_idcs(term_idcs<=numel(free_tmp{ii}.term));
+                free_tmp{ii}.term = free_tmp{ii}.term(term_idcs_ii);
+            end
+        end
+        val.free = free_tmp(eq_idcs);
+    else
+        % % The input PDE describes actual equations
+        % % --> we'll have to move the terms into the field 'free'.
+        % Equations can be extracted of each of the following types.
+        objs_arr = {'x';'y';'z';'BC'};
+        % Get the number of equations of each type.
+        neqs_arr = size(PDE,objs_arr);
+        nneqs_arr = cumsum([0;neqs_arr]);
+        % Initiale a cell of free equations for the output.
+        free_tmp = cell(length(eq_idcs),1);
+        % Allow the left-hand side of the equation to be extracted as well.
+        if any(term_idcs==0)
+            if numel(term_idcs)~=1
+                error("When extracting term number 0, nu other terms can be extracted.")
+            % elseif any(eq_idcs)>nneqs_arr(end-1)
+            %     error("Left-hand side of boundary conditions cannot be extracted.")
+            end
+        end
+        for ii=1:numel(free_tmp)
+            % Determine which object equation ii corresponds to
+            obj_idx = find(eq_idcs(ii)<=nneqs_arr,1,'first')-1;
+            obj = objs_arr{obj_idx};
+            eq_num = eq_idcs(ii)-nneqs_arr(obj_idx);
+            % Extract size and variable information on the equation
+            eq_struct = PDE.(obj){eq_num};
+            if isfield(eq_struct,'size')
+                free_tmp{ii}.size = eq_struct.size;
+            end
+            if isfield(eq_struct,'vars')
+                free_tmp{ii}.vars = eq_struct.vars;
+            end
+            free_tmp{ii}.term = {};
+            % Extract the desired terms from the equation
+            if all(term_idcs==0)
+                % Extract left-hand side of equation.
+                if obj_idx==4
+                    % Left-hand side of boundary condition is zero.
+                    continue
+                else
+                    % Left-hand side of output or PDE is just the output or
+                    % state.
+                    free_tmp{ii}.term{1}.(obj) = eq_num;
+                    free_tmp{ii}.term{1}.C = eye(PDE.(obj){eq_num}.size);
+                    if isfield(eq_struct,'tdiff')
+                        free_tmp{ii}.term{1}.tdiff = eq_struct.tdiff;
+                    end
+                end
+            else
+                if isfield(eq_struct,'term') && ~isempty(eq_struct.term)
+                    % Retain only terms specified by term_idcs.
+                    term_idcs_ii = term_idcs(term_idcs<=numel(eq_struct.term));
+                    free_tmp{ii}.term = eq_struct.term(term_idcs_ii);
+                end
+            end
+        end
+        % % Remove all non-free equations.
+        for ii=1:nneqs_arr(end-1)
+            % Determine which object equation ii corresponds to
+            obj_idx = find(ii<=nneqs_arr,1,'first')-1;
+            obj = objs_arr{obj_idx};
+            eq_num = ii-nneqs_arr(obj_idx);
+            % Remove all terms from the equation.
+            % if isfield(val.(obj){eq_num},'term')
+            %     val.(obj){eq_num} = rmfield(val.(obj){eq_num},'term');
+            % end%
+            val.(obj){eq_num}.term = {};
+        end
+        val.BC = cell(0,1);
+        % % Finally, set the free equations in the PDE.
+        val.free = free_tmp;
+        val.is_initialized = false;
+    end
+    % Perform remaining subsref.
+    if numel(prop)>1
+        val = subsref(val,prop(2:end));
+    end
+
+elseif strcmp(prop(1).type,'.')
+    % % % Allow,size, variables and domain of PDE variable to be extracted.
+    if is_var && (strcmp(prop(1).subs,'size') || strcmp(prop(1).subs,'vars') || strcmp(prop(1).subs,'dom'))
+        val = PDE.(obj){1}.(prop(1).subs);
+        % Perform remaining subsref.
+        if numel(prop)>1
+            val = builtin('subsref',val,prop(2:end));
+        end
+        return
+    end
+    % % % Allow coefficients to be retrieved by calling PDE.C{i,j}
+    if strcmpi(prop(1).subs,'C')
+        % Deal with case that the PDE corresponds to a single term.
+        sz_PDE = size(PDE);
+        if all(sz_PDE==1) && (numel(prop)==1 || strcmp(prop(2).type,'()'))
+            % Allow coefficient C to be extracted in case of a single term.
+            prop_new = prop(1);
+            prop_new(2).type = '{}';        % Extract coefficients {1,1};
+            prop_new(2).subs = {1,1};
+            prop_new = [prop_new,prop(2:end)];
+            prop = prop_new;
+        elseif numel(prop)==1
+            error('To extract the coefficients C_{ij} appearing in term j of equation i, call "PDE.C{i,j}".')
+        end
         if ~strcmp(prop(2).type,'{}')
             error('To extract the coefficients C_{ij} appearing in term j of equation i, call "PDE.C{i,j}".')
         elseif numel(prop(2).subs)<2
@@ -68,9 +227,20 @@ else
             ii = prop(2).subs{1};
             jj = prop(2).subs{2};
         end
-        % Distinguish between dynamics, outputs, or BCs
+        % Check that the equation and term number make sense.
         if ii <= 0 || ii~=round(ii) || ii~=real(ii)
             error('Equation number i must be specified as real, nonnegative integer when extracting coefficients "PDE.C{i,j}".')
+        elseif jj <= 0 || jj~=round(jj) || jj~=real(jj)
+            error('Term number j must be specified as real, nonnegative integer when extracting coefficients "PDE.C{i,j}".')
+        elseif ii>sz_PDE(1)
+            error("Equation number i cannot exceed number of equations in the PDE, when extracting coefficients 'PDE.C{i,j}'.")
+        elseif jj>sz_PDE(2)
+            error("Term number j cannot exceed maximum number of terms in each equation, when extracting coefficients 'PDE.C{i,j}'.")
+        end
+        % Distinguish between free equation, dynamics, outputs, or BCs
+        if ~isempty(PDE.free)
+            eq = PDE.free{ii};
+            eqname = ['free{',num2str(ii),'}'];
         elseif ii <= numel(PDE.x)
             % Coefficients in PDE dynamics are requested.
             eq = PDE.x{ii};
@@ -100,6 +270,10 @@ else
             error(['The proposed term index exceeds the number of terms in the equation "PDE.',eqname,'".'])
         else
             val = eq.term{jj}.C;
+        end
+        % Perform remaining subsref.
+        if numel(prop)>2
+            val = builtin('subsref',val,prop(3:end));
         end
     else
         % Otherwise, we have nothing fancy implemented at this time.
