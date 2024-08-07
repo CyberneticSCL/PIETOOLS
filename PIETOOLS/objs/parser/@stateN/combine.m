4fbf387e85c57548642404109c92b68df8818170
Sachin <sachin.shivakumar@asu.edu>
2024-08-07 16:36:31 -0500

    Update combine.m
    Bug in combine which used state vector length instead of multiplier dimension.

--- PIETOOLS/objs/parser/@stateN/combine.m
+++ PIETOOLS/objs/parser/@stateN/combine.m
@@ -1,2 +1,31 @@
-function out = combine(varargin)
+function [out, varargout] = combine(varargin)
+out = varargin{1};
+for i=2:nargin
+    tmp = varargin{i};
+    for j=1:length(tmp)
+        s.type = '()'; s.subs = {j};
+        temp = subsref(tmp,s); 
+        if ~ismember(temp,out)
+            out = [out; temp];
+        end
+    end
+end
+
+varargout = cell(1,nargin);
+
+for i=1:nargin
+    varargout{i} = findPermutation(out,varargin{i});
+end
+end
+function P = findPermutation(A,B) % returns P, such that B = P*A
+s.type = '.'; s.subs = 'multiplier';
+Blens = cellfun('size',subsref(B,s),1);
+Alens = cellfun('size',subsref(A,s),1);
+P = zeros(sum(Blens),sum(Alens));
+blen = [0;cumsum(Blens)]+1; alen = [0;cumsum(Alens)]+1;
+[~,idx] = ismember(B,A);
+for i=1:length(blen)-1
+    tmp = subsref(A,s);
+    P(blen(i):blen(i+1)-1,alen(idx(i)):alen(idx(i)+1)-1) = eye(tmp(idx(i)));
+end
 end
