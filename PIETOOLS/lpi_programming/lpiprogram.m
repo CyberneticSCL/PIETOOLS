ed7e3f83a29d95c29c9837ab629383b722ca5e52
Declan <91631433+djagt@users.noreply.github.com>
2024-11-02 15:02:42 -0700

    Update lpiprogram.m
    The function now requires only primary spatial variables to be specified as the first argument, rather than also the dummy variables used for integration. If only a column or row vector of variables is passed as the first argument, the function assumes only primary spatial variables are specified, and generates associated dummy variables itself by taking the spatial variable names and adding '_dum'. Note that there is a bit of ambiguity if variables are passed as a 1x2 row array `vartable=[x,y]`, in which case it is not obvious whether `x` and `y` are separate spatial variables, or if `x` is a spatial variable and `y` is a dummy variable. In this setting, the function relies on the domain `dom` to be properly specified, so that if `dom` has 1 row it assumes `y` is a dummy variable, and if `dom` has two rows it assumes `y` is a primary spatial variable on domain `dom(2,:)`.

--- PIETOOLS/lpi_programming/lpiprogram.m
+++ PIETOOLS/lpi_programming/lpiprogram.m
@@ -8,7 +8,9 @@ function prog = lpiprogram(vartable,dom,freevartable,decvartable)
 % - vartable:   nx2 array of type 'polynomial', specifying n independent
 %               variables for the optimization program in the first column,
 %               along with associated dummy variables used for integration
-%               in the second column.
+%               in the second column. If an nx1 array of spatial variables
+%               is provided, associated dummy variables will be generated,
+%               so that if vartable(i,1)=s1, then vartable(1,2) = s1_dum;
 % - dom:        nx2 array of type 'double', with each row dom(i,:)=[1,b]
 %               specifying the interval on which the independent variable
 %               vartable(i,1) (and corresponding dummy variable) is
@@ -69,7 +71,13 @@ elseif ~isa(vartable,'polynomial')
 elseif ~ispvar(vartable)
     error("Each element of the array of spatial variables should correspond to a single polynomial variable.")
 elseif size(vartable,2)~=2
-    error("Spatial variables should be specified as nx2 array, with first column specifying primary variables, and second column dummy variables used for integration.")
+    if size(vartable,1)==1
+        % Assume a row vector of only primary spatial variables is
+        % specified.
+        vartable = vartable';
+    elseif size(vartable,2)~=1
+        error("Spatial variables should be specified as nx2 array, with first column specifying primary variables, and second column dummy variables used for integration.")
+    end
 end
 if size(vartable,1)>2
     error('LPI programs involving more than 2 spatial variables are currently not supported.')
@@ -86,8 +94,16 @@ elseif ~isa(dom,'double') && ~(isa(dom,'polynomial') && isdouble(dom))
 elseif size(dom,2)~=2
     error("Spatial domain should be specified as nx2 array.")
 elseif size(dom,1)~=size(vartable,1)
-    if size(dom,1)==1
-        % Assume same domain for all spatial variables.
+    % The number of domains should match the number of spatial variables,
+    % but we have to be careful that the user may specify [s1,s2] as two
+    % spatial variables (on different domains), or [s1,th2] as spatial and
+    % dummy variable (on same domain).
+    if size(dom,1)==2 && all(size(vartable)==[1,2])
+        % Assume the user declares two spatial variables, [s1,s2];
+        vartable = vartable';
+    elseif size(dom,1)==1
+        % Assume all rows of vartable specify different spatial variables, 
+        % but set same domain for all spatial variables.
         dom = repmat(dom,[size(vartable,1),1]);
     else
         error("Number of rows in domain array should match number of spatial variables.")
@@ -97,6 +113,15 @@ dom = double(dom);
 % Make sure the domain makes sense.
 if any(dom(:,2)<=dom(:,1))
     error('Upper boundary of domain should be strictly greater than lower boundary.')
+end
+
+% % If only spatial variables have been declared, add dummy variables:
+if size(vartable,2)==1
+    dum_vars = cell(size(vartable,1),1);
+    for ii=1:size(vartable,1)
+        dum_vars{ii} = [vartable(ii,1).varname{1},'_dum'];
+    end
+    vartable = [vartable, polynomial(dum_vars)];
 end
 
 % % Check that the "free" variables and decision variables are 
