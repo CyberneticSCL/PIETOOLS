17cc2be76b789765bbfc6990986f6cc1231f9f60
Yulia <ypeet@asu.edu>
2024-12-21 18:37:42 -0700

    Add files via upload

---
+++ PIETOOLS/PIESIM/PIESIM_routines/Discretization_Of_Operators/PIESIM_PIop_discretize.m
@@ -0,0 +1,45 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% PIESIM_PIop_discretize.m     PIETOOLS 2024
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Constructs Rcheb - PI operator evaluated on a Chebyshev grid
+%
+% Inputs:
+% R - PI operator to be discretized
+% maxdeg - maximum polynomial degree of a PI operator R
+%
+% Output:
+% Rcheb - a square matrix of size (maxdeg+2)x(maxdeg+2) that evaluates the PI operator
+% on a Chebysheb grid
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% If you modify this code, document all changes carefully and include date
+% authorship, and a brief description of modifications
+%
+% Initial coding YP  - 12_21_2024
+function Rcheb=PIESIM_PIop_discretize(R,maxdeg)
+
+chebgrid=cos(pi*(0:maxdeg+1)/(maxdeg+1));
+if isa(R,'polynomial')
+    switch(R.nvars)
+        case 2
+        % Two variables present
+            Reval=subs(subs(R,R.varname(2),chebgrid),R.varname(1),chebgrid);
+        case 1
+        % One variable present
+            Re=subs(R,R.varname,chebgrid);
+            var=cell2mat(R.varname);
+            if (length(var)<=2)
+                Reval=repmat(Re',1,maxdeg+2);
+            else
+                Reval=repmat(Re,maxdeg+2,1);
+            end
+        case 0 
+            % Scalar polynomial
+            Reval=R*ones(maxdeg+2);
+end
+else
+    Reval=R*ones(maxdeg+2);
+end
+
+Reval=double(Reval);
+Rcheb=fcgltran2d(Reval,1);
