528178151f5f24e0d6aa32a3cbdba8c51c67e4cc
Declan <91631433+djagt@users.noreply.github.com>
2025-01-06 10:25:13 -0700

    Update command line format examples
    Just minor changes.

--- PIETOOLS/PIETOOLS_examples/Examples_Library/command_line_input_Examples/stability_2.m
+++ PIETOOLS/PIETOOLS_examples/Examples_Library/command_line_input_Examples/stability_2.m
@@ -20,21 +20,21 @@ display(pie);
 
 
 
-%% Define dependent variables and system variable
-%	PDE: x_{t} = Fm*x - Lm*x_{s}                    | Different parameters          Lamare 2016 [1] 
-%   BCs: [x_-(s=1)] = [Gm1, Gm2] [x_-(s=0)]         | may be invoked calling
-%        [x_+(s=0)] = [Gm3, Gm4] [x_+(s=1)]         | examples 2.1, 2.2, 
-%                                                   | 2.3, and 2.4.
-x = state('pde',2); % x = [x_-; x_+];
-pde = sys();
-Gm1=[.2]; Gm2=[-.3]; Gm3=[.6]; Gm4=[.1]; Lm=[-3 0;0 1]; Fm=[.2 -.3; .6 .1];
-%% Define equations
-eq_PDE = diff(x,t)==Fm*x-Lm*diff(x,s); % 	PDE: x_{t} = Fm*x - Lm*x_{s}
-eq_BC = [[1,0]*subs(x,s,1); [0,1]*subs(x,s,0)]==[Gm1,Gm2;Gm3,Gm4]*[[1,0]*subs(x,s,0);[0,1]*subs(x,s,1)];    
-%   BCs: [x_-(s=1)] = [Gm1, Gm2] [x_-(s=0)], [x_+(s=0)] = [Gm3, Gm4] [x_+(s=1)]
-%% addequations to pde system; set control inputs/observed inputs, if any
-pde = addequation(pde,[eq_PDE;eq_BC]);
-%% display pde to verify and convert to pie
-display(pde);
-pie = convert(pde,'pie');
-display(pie);
+% %% Define dependent variables and system variable
+% %	PDE: x_{t} = Fm*x - Lm*x_{s}                    | Different parameters          Lamare 2016 [1] 
+% %   BCs: [x_-(s=1)] = [Gm1, Gm2] [x_-(s=0)]         | may be invoked calling
+% %        [x_+(s=0)] = [Gm3, Gm4] [x_+(s=1)]         | examples 2.1, 2.2, 
+% %                                                   | 2.3, and 2.4.
+% x = state('pde',2); % x = [x_-; x_+];
+% pde = sys();
+% Gm1=[.2]; Gm2=[-.3]; Gm3=[.6]; Gm4=[.1]; Lm=[-3 0;0 1]; Fm=[.2 -.3; .6 .1];
+% %% Define equations
+% eq_PDE = diff(x,t)==Fm*x-Lm*diff(x,s); % 	PDE: x_{t} = Fm*x - Lm*x_{s}
+% eq_BC = [[1,0]*subs(x,s,1); [0,1]*subs(x,s,0)]==[Gm1,Gm2;Gm3,Gm4]*[[1,0]*subs(x,s,0);[0,1]*subs(x,s,1)];    
+% %   BCs: [x_-(s=1)] = [Gm1, Gm2] [x_-(s=0)], [x_+(s=0)] = [Gm3, Gm4] [x_+(s=1)]
+% %% addequations to pde system; set control inputs/observed inputs, if any
+% pde = addequation(pde,[eq_PDE;eq_BC]);
+% %% display pde to verify and convert to pie
+% display(pde);
+% pie = convert(pde,'pie');
+% display(pie);
--- PIETOOLS/PIETOOLS_examples/Examples_Library/command_line_input_Examples/stability_3.m
+++ PIETOOLS/PIETOOLS_examples/Examples_Library/command_line_input_Examples/stability_3.m
@@ -5,9 +5,7 @@ pvar s t; % define independent variables
 %   BCs: [x_+(s=0)] = [K00, K01] [x_+(s=1)]         | K10 and K11 may be
 %        [x_-(s=1)] = [K10, K11] [x_-(s=0)]         | specified
 %   The implementation is based on a Linearized Saint–Venant–Exner Model
-x1 = pde_var(s,[0,1]);
-x2 = pde_var(s,[0,1]);
-x3 = pde_var(s,[0,1]);
+x = pde_var(3,s,[0,1]); % x = [x_+; x_-];
 
  Hs = 1;       Vs = 1;     Bs = 0.1;
  g = 10;       Cf = 1;
@@ -33,17 +31,62 @@ x3 = pde_var(s,[0,1]);
  K10 = K(2:3,1); K11 = K(2:3,2:3);
 
 %% Define equations
-eq_PDE = [diff(x1,t)==Mm(1,:)*[x1;x2;x3]-Lm(1,:)*diff([x1;x2;x3],s);
-          diff(x2,t)==Mm(2,:)*[x1;x2;x3]-Lm(2,:)*diff([x1;x2;x3],s);
-          diff(x3,t)==Mm(3,:)*[x1;x2;x3]-Lm(3,:)*diff([x1;x2;x3],s)]; % 	PDE: x_{t} = Mm*x - Lm*x_{s}
-eq_BC = [subs(x1,s,0); subs([x2;x3],s,1)]==[K00,K01;K10,K11]*[subs([x1;x2],s,1);subs(x3,s,0)];    
+eq_PDE = diff(x,t)==Mm*x-Lm*diff(x,s); % 	PDE: x_{t} = Mm*x - Lm*x_{s}
+eq_BC = [[1,0,0]*subs(x,s,0); [0,1,0;0,0,1]*subs(x,s,1)]==[K00,K01;K10,K11]*[[1,0,0;0,1,0]*subs(x,s,1);[0,0,1]*subs(x,s,0)];    
 %   BCs: [x_+(s=0)] = [K00, K01] [x_+(s=1)], [x_-(s=1)] = [K10, K11] [x_-(s=0)]
-%% initialize pde system;
+%% addequations to pde system; set control inputs/observed inputs, if any
 pde = initialize([eq_PDE;eq_BC]);
 %% display pde to verify and convert to pie
 display(pde);
 pie = convert(pde,'pie');
 display(pie);
+
+
+
+% %% Define dependent variables and system variable
+% %   PDE: x_{t} = Mm*x - Lm*x_{s}                	| Mm, Lm, K00, K01,             Diagne 2012 [2] 
+% %   BCs: [x_+(s=0)] = [K00, K01] [x_+(s=1)]         | K10 and K11 may be
+% %        [x_-(s=1)] = [K10, K11] [x_-(s=0)]         | specified
+% %   The implementation is based on a Linearized Saint–Venant–Exner Model
+% x1 = pde_var(s,[0,1]);
+% x2 = pde_var(s,[0,1]);
+% x3 = pde_var(s,[0,1]);
+% 
+%  Hs = 1;       Vs = 1;     Bs = 0.1;
+%  g = 10;       Cf = 1;
+%  Lm = diag(sort(eig([Vs,Hs,0;10,Vs,10;0,1*Vs^2,0])));
+%  lm1 = Lm(1,1);    lm2 = Lm(2,2);      lm3 = Lm(3,3);
+%  al1 = Cf*(3*Vs - 2*lm1) * Vs/Hs * lm1/((lm1-lm2)*(lm1-lm3));
+%  al2 = Cf*(3*Vs - 2*lm2) * Vs/Hs * lm2/((lm2-lm3)*(lm2-lm1));
+%  al3 = Cf*(3*Vs - 2*lm3) * Vs/Hs * lm3/((lm3-lm1)*(lm3-lm2));
+%  Mm = [al1*ones(3,1) , al2*ones(3,1) , al3*ones(3,1)];
+%  a21 = (lm1 - lm2) * (1+ ((lm1-Vs) * (lm2-Vs))/(g*Hs));
+%  a32 = (lm2 - lm3) * (1+ ((lm2-Vs) * (lm3-Vs))/(g*Hs));
+%  a13 = (lm3 - lm1) * (1+ ((lm3-Vs) * (lm1-Vs))/(g*Hs));
+%  c21 = (lm3/g) * (lm1 - lm2);
+%  c32 = (lm1/g) * (lm2 - lm3);
+%  c13 = (lm2/g) * (lm3 - lm1);
+%  k1 = 1;       k2 = 2;
+%  pi2 = (a21 - c21*k1)/(a32 - c32*k1);
+%  pi3 = (a13 - c13*k1)/(a32 - c32*k1);
+%  chi2 = ((lm2 - Vs)/(lm1 - Vs)) * ((g + (lm2-Vs)*k2)/(g + (lm1-Vs)*k2));
+%  chi3 = ((lm3 - Vs)/(lm1 - Vs)) * ((g + (lm3-Vs)*k2)/(g + (lm1-Vs)*k2));
+%  K = [0,chi2,chi3; pi2,0,0; pi3,0,0];
+%  K00 = K(1,1);   K01 = K(1,2:3);
+%  K10 = K(2:3,1); K11 = K(2:3,2:3);
+% 
+% %% Define equations
+% eq_PDE = [diff(x1,t)==Mm(1,:)*[x1;x2;x3]-Lm(1,:)*diff([x1;x2;x3],s);
+%           diff(x2,t)==Mm(2,:)*[x1;x2;x3]-Lm(2,:)*diff([x1;x2;x3],s);
+%           diff(x3,t)==Mm(3,:)*[x1;x2;x3]-Lm(3,:)*diff([x1;x2;x3],s)]; % 	PDE: x_{t} = Mm*x - Lm*x_{s}
+% eq_BC = [subs(x1,s,0); subs([x2;x3],s,1)]==[K00,K01;K10,K11]*[subs([x1;x2],s,1);subs(x3,s,0)];    
+% %   BCs: [x_+(s=0)] = [K00, K01] [x_+(s=1)], [x_-(s=1)] = [K10, K11] [x_-(s=0)]
+% %% initialize pde system;
+% pde = initialize([eq_PDE;eq_BC]);
+% %% display pde to verify and convert to pie
+% display(pde);
+% pie = convert(pde,'pie');
+% display(pie);
 
 
 
