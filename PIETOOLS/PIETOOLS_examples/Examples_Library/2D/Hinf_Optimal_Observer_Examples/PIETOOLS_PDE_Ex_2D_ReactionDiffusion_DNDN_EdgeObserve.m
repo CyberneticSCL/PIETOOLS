006d40eb5eb030aa882b0bb323a025884f1258d7
Declan <91631433+djagt@users.noreply.github.com>
2025-01-05 21:07:46 -0700

    Update 2D PDE examples.
    The remaining examples have been updated to declare the PDE using `pde_var`.

--- PIETOOLS/PIETOOLS_examples/Examples_Library/2D/Hinf_Optimal_Observer_Examples/PIETOOLS_PDE_Ex_2D_ReactionDiffusion_DNDN_EdgeObserve.m
+++ PIETOOLS/PIETOOLS_examples/Examples_Library/2D/Hinf_Optimal_Observer_Examples/PIETOOLS_PDE_Ex_2D_ReactionDiffusion_DNDN_EdgeObserve.m
@@ -53,43 +53,60 @@ end
 
 
 % % % Construct the PDE.
-%%% Term-based input format
-PDE_t.x{1}.vars = [s1;s2];      PDE_t.x{1}.dom = [a,b;c,d];
-PDE_t.x{1}.size = ne;
-PDE_t.w{1}.vars = [];           PDE_t.w{1}.size = ne;
-PDE_t.w{2}.vars = [];           PDE_t.w{2}.size = ne;
-PDE_t.w{3}.vars = [];           PDE_t.w{3}.size = ne;
-PDE_t.y{1}.vars = s1;           PDE_t.y{1}.dom = [a,b];
-PDE_t.y{1}.size = ne;
-PDE_t.y{2}.vars = s2;           PDE_t.y{2}.dom = [c,d];
-PDE_t.y{2}.size = ne;
-PDE_t.z{1}.vars = [];           PDE_t.z{1}.size = ne;
+%%% pde_var input format
+clear stateNameGenerator
+x = pde_var(ne,[s1;s2],[a,b;c,d]);
+w1 = pde_var('in',ne);
+w2 = pde_var('in',ne);
+w3 = pde_var('in',ne);
+y1 = pde_var('sense',ne,s1,[a,b]);
+y2 = pde_var('sense',ne,s2,[c,d]);
+z = pde_var('out',ne);
+PDE_t = [diff(x,'t')==r*x+nu*(diff(x,s1,2)+diff(x,s2,2))+Cw*w1;
+         y1==int(x,s2,[c,d])+w2;
+         y2==int(x,s1,[a,b])+w3;
+         z==int(x,[s1;s2],[a,b;c,d]);
+         subs(x,s1,a)==0;       subs(diff(x,s1),s1,b)==0;
+         subs(x,s2,c)==0;       subs(diff(x,s2),s2,d)==0];
 
-% PDE: x_{t} = [r, c1, c2] * [x; x_{s1s1}; x_{s2s2}] + (s1^2-1)*(s2^2-1)*w
-PDE_t.x{1}.term{1}.D = [0,0; 2,0; 0,2];
-PDE_t.x{1}.term{1}.C = [r*eye(ne), nu*eye(ne), nu*eye(ne)];
 
-PDE_t.x{1}.term{2}.w = 1;
-PDE_t.x{1}.term{2}.C = Cw;
-
-% OUT: y2(t,s2) = x(t,s1=b,s2) + w3;
-PDE_t.y{2}.term{1}.x = 1;   PDE_t.y{2}.term{1}.loc = [b,s2];
-PDE_t.y{2}.term{2}.w = 3;
-
-% OUT: y1(t,s1) = x(t,s1,s2=d) + w2;
-PDE_t.y{1}.term{1}.x = 1;   PDE_t.y{1}.term{1}.loc = [s1,d];
-PDE_t.y{1}.term{2}.w = 2;
-
-% OUT: z(t) = int_{a}^{b} int_{c}^{d} x(t,s1,s2) ds2 ds1
-PDE_t.z{1}.term{1}.x = 1;
-PDE_t.z{1}.term{1}.I = {[a,b];[c,d]};
-
-% BC1: 0 = x(s1,c)                     % BC3: 0 = x_{s2}(s1,d)
-PDE_t.BC{1}.term{1}.loc = [s1,c];      PDE_t.BC{3}.term{1}.loc = [s1,d];
-                                       PDE_t.BC{3}.term{1}.D = [0,1];
-% BC2: 0 = x(a,s2)                     % BC4: 0 = x_{s1}(b,s2)
-PDE_t.BC{2}.term{1}.loc = [a,s2];      PDE_t.BC{4}.term{1}.loc = [b,s2];
-                                       PDE_t.BC{4}.term{1}.D = [1,0];
+% %%% Term-based input format
+% PDE_t.x{1}.vars = [s1;s2];      PDE_t.x{1}.dom = [a,b;c,d];
+% PDE_t.x{1}.size = ne;
+% PDE_t.w{1}.vars = [];           PDE_t.w{1}.size = ne;
+% PDE_t.w{2}.vars = [];           PDE_t.w{2}.size = ne;
+% PDE_t.w{3}.vars = [];           PDE_t.w{3}.size = ne;
+% PDE_t.y{1}.vars = s1;           PDE_t.y{1}.dom = [a,b];
+% PDE_t.y{1}.size = ne;
+% PDE_t.y{2}.vars = s2;           PDE_t.y{2}.dom = [c,d];
+% PDE_t.y{2}.size = ne;
+% PDE_t.z{1}.vars = [];           PDE_t.z{1}.size = ne;
+% 
+% % PDE: x_{t} = [r, c1, c2] * [x; x_{s1s1}; x_{s2s2}] + (s1^2-1)*(s2^2-1)*w
+% PDE_t.x{1}.term{1}.D = [0,0; 2,0; 0,2];
+% PDE_t.x{1}.term{1}.C = [r*eye(ne), nu*eye(ne), nu*eye(ne)];
+% 
+% PDE_t.x{1}.term{2}.w = 1;
+% PDE_t.x{1}.term{2}.C = Cw;
+% 
+% % OUT: y2(t,s2) = x(t,s1=b,s2) + w3;
+% PDE_t.y{2}.term{1}.x = 1;   PDE_t.y{2}.term{1}.loc = [b,s2];
+% PDE_t.y{2}.term{2}.w = 3;
+% 
+% % OUT: y1(t,s1) = x(t,s1,s2=d) + w2;
+% PDE_t.y{1}.term{1}.x = 1;   PDE_t.y{1}.term{1}.loc = [s1,d];
+% PDE_t.y{1}.term{2}.w = 2;
+% 
+% % OUT: z(t) = int_{a}^{b} int_{c}^{d} x(t,s1,s2) ds2 ds1
+% PDE_t.z{1}.term{1}.x = 1;
+% PDE_t.z{1}.term{1}.I = {[a,b];[c,d]};
+% 
+% % BC1: 0 = x(s1,c)                     % BC3: 0 = x_{s2}(s1,d)
+% PDE_t.BC{1}.term{1}.loc = [s1,c];      PDE_t.BC{3}.term{1}.loc = [s1,d];
+%                                        PDE_t.BC{3}.term{1}.D = [0,1];
+% % BC2: 0 = x(a,s2)                     % BC4: 0 = x_{s1}(b,s2)
+% PDE_t.BC{2}.term{1}.loc = [a,s2];      PDE_t.BC{4}.term{1}.loc = [b,s2];
+%                                        PDE_t.BC{4}.term{1}.D = [1,0];
 
 
 if GUI~=0
