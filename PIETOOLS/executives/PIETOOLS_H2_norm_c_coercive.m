bc981899d9a61789b2b2c1b07475e3e43e500d0d
Declan <91631433+djagt@users.noreply.github.com>
2025-01-06 21:14:14 -0700

    Update H2 norm dual executive.
    The function now solves the non-coercive version of the LPI. The coercive version is still available as a separate function.

--- PIETOOLS/executives/PIETOOLS_H2_norm_c_coercive.m
+++ PIETOOLS/executives/PIETOOLS_H2_norm_c_coercive.m
@@ -1,4 +1,4 @@
-function [prog,W,gam,R,Q]= PIETOOLS_H2_norm_c(PIE, settings,options)
+function [prog,Wc, gam]= PIETOOLS_H2_norm_c(PIE, settings,options)
 % This function solves the minimization problem to obtain the H2 norm of a linear distributed parameter
 % system using the controlability gramian approach and PIE framework. For
 % the feasibility test an additional input with a assigned value to the
@@ -7,12 +7,9 @@ function [prog,W,gam,R,Q]= PIETOOLS_H2_norm_c(PIE, settings,options)
 %   PIE : PIE structure of the corresponding system
 %   settings : options related to PIETOOLS
 % outputs: 
-%   h2 = computed H2 norm for SDP problem or optional input value for feasibility tests;
-%   W = controlability gramian, a positive PI operator;
-%   prog = sum of squares program information;
-%   R = positive PI operator parameterizing the non-coercive Lyapunov 
-%       functional V(v)=<v,R*v>;
-%   Q = indefinite PI operator such that R=PIE.T*Q=Q'*PIE.T';
+%   h2= computed H2 norm for SDP problem or optional input value for feasibility tests;
+%   W= controlability gramian, a positive PI operator;
+%  prog= sum of squares program information;
 %
 % NOTES:
 % For support, contact M. Peet, Arizona State University at mpeet@asu.edu,
@@ -20,7 +17,7 @@ function [prog,W,gam,R,Q]= PIETOOLS_H2_norm_c(PIE, settings,options)
 % Braghini d166353@dac.unicamp.br.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Copyright (C)2024 PIETOOLS Team
+% Copyright (C)2022  M. Peet, S. Shivakumar, D. Jagt, D. Braghini
 %
 % This program is free software; you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
@@ -41,7 +38,6 @@ function [prog,W,gam,R,Q]= PIETOOLS_H2_norm_c(PIE, settings,options)
 % If you modify this code, document all changes carefully and include date
 % authorship, and a brief description of modifications
 % DJ - 10/19/2024: Update to use new LPI programming structure;
-% DJ - 01/06/2024: Update to non-coercive version;
 
 
 % Check if the PIE is properly specified.
@@ -53,11 +49,11 @@ end
 % Pass to the 2D executive if necessary.
 if PIE.dim==2
     if nargin==1
-        [prog, W, gam] = PIETOOLS_H2_norm_2D_c(PIE);
+        [prog,Wc, gam] = PIETOOLS_H2_norm_2D_c(PIE);
     elseif nargin==2
-        [prog, W, gam] = PIETOOLS_H2_norm_2D_c(PIE, settings);
+        [prog,Wc, gam] = PIETOOLS_H2_norm_2D_c(PIE, settings);
     else
-        [prog, W, gam] = PIETOOLS_H2_norm_2D_c(PIE, settings, options);
+        [prog,Wc, gam] = PIETOOLS_H2_norm_2D_c(PIE, settings, options);
     end
     return
 end
@@ -115,39 +111,25 @@ end
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% STEP 1: declare the posopvar variable, Rop, which defines the storage 
-% function candidate V(v)=<v,Rop*v>=<Top'*v,Wop*Top'*v>
+% STEP 1: declare the posopvar variable, Pop, which defines the storage 
+% function candidate
 disp('- Declaring Gramian using specified options...');
 
-[prog, R1op] = poslpivar(prog, Top.dim, dd1, options1);
+[prog, P1op] = poslpivar(prog, Top.dim, dd1, options1);
 
 if override1~=1
     [prog, P2op] = poslpivar(prog, Top.dim, dd12, options12);
-    Rop=R1op+P2op;
+    Wop=P1op+P2op;
 else
-    Rop=R1op;
+    Wop=P1op;
 end
-
-% Also declare an indefinite operator Qop=Wop*Top' so that Rop = Top*Qop.   % DJ, 01/06/2025
-Qdeg = get_lpivar_degs(Rop,Top);
-[prog, Qop] = lpivar(prog,Top.dim,Qdeg);
-prog = lpi_eq(prog, Top*Qop-Rop);
-
-% Finally, declare a positive operator (matrix) Wm representing the
-% controllability Gramian
-[prog, Wm] = poslpivar(prog,Czop.dim(:,1));
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % STEP 2: Using the controlability gramian
 
 disp('- Constructing the Negativity Constraint...');
-Iw = mat2opvar(eye(size(Bwop,2)), Bwop.dim(:,2), PIE.vars, PIE.dom);
 
-Dneg = [-gam*Iw   Bwop'
-        Bwop      Qop'*Aop'+Aop*Qop];
-Dpos = [Wm           Czop*Qop
-        Qop'*Czop'   Rop];
+Dop =  (Aop*Wop)*Top'+Top*(Wop*Aop')+Bwop*Bwop';
     
     
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -158,38 +140,33 @@ Dpos = [Wm           Czop*Qop
 disp('- Enforcing the Inequalities Constraints...');
 if sosineq_on
     disp('  - Using lpi_ineq...');
-    prog = lpi_ineq(prog,-Dneg,opts);
-    prog = lpi_ineq(prog,Dpos,opts);
+    prog = lpi_ineq(prog,-Dop,opts);
 else
     disp('  - Using an Equality constraint...');
-    [prog, De1op] = poslpivar(prog, Dneg.dim, dd2, options2);
-    [prog, De3op] = poslpivar(prog, Dpos.dim, dd2, options2);
+    [prog, De1op] = poslpivar(prog, Dop.dim, dd2, options2);
     
     if override2~=1
-        [prog, De2op] = poslpivar(prog, Dneg.dim, dd3, options3);
+        [prog, De2op] = poslpivar(prog, Dop.dim, dd3, options3);
         Deop=De1op+De2op;
-         [prog, De4op] = poslpivar(prog, Dpos.dim, dd3, options3);
-        Deopp=De3op+De4op;      
     else
         Deop=De1op;
-        Deopp=De3op;
     end
-    prog = lpi_eq(prog,Deop+Dneg,'symmetric'); %Dneg=-Deop
-    prog = lpi_eq(prog,Deopp-Dpos,'symmetric'); %Dpos=Deopp
+    prog = lpi_eq(prog,Deop+Dop,'symmetric'); %Dop=-Deop
 end
 
+tempObj = Czop*Wop*Czop';
+tempMat = tempObj.P;
+traceVal=0;
+for idx = 1:size(tempMat,1)
+    traceVal = traceVal+tempMat(idx,idx);
+end
 % ensuring scalar inequality gam>trace
-traceVal = trace(Wm.P);
 prog = lpi_ineq(prog, gam-traceVal);
 
 %solving the sos program
 disp('- Solving the LPI using the specified SDP solver...');
 prog = lpisolve(prog,sos_opts); 
-
-W = lpigetsol(prog,Wm);
-R = lpigetsol(prog,Rop);
-Q = lpigetsol(prog,Qop);
-
+Wc = lpigetsol(prog,Wop);
 if nargin<=2 || ~isfield(options,'h2')
         gam = sqrt(double(lpigetsol(prog,gam)));
         disp('The H2 norm of the given system is upper bounded by:')
