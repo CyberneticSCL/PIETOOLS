7c435804d7f667f046b463a4a9a79ec991d142db
Declan <91631433+djagt@users.noreply.github.com>
2025-01-06 17:12:48 -0700

    Create pietools_path_update.m
    The script updates the current path to include all PIETOOLS directories and subdirectories.

---
+++ PIETOOLS/pietools_path_update.m
@@ -0,0 +1,5 @@
+% This script updates the MATLAB path to include all PIETOOLS directories
+% and subdirectories. Save your path after running to keep directories on
+% path for future sessions. Don't move this file!
+current_folder = pwd; 
+addpath(genpath(current_folder));
