9aa40686161540de96055e760c3df1219b339860
Yulia <ypeet@asu.edu>
2025-06-01 21:54:50 -0700

    Add files via upload

--- PIETOOLS/PIESIM/PIESIM_routines/Discretization_Of_Operators/PIESIM_4PI2Mat_cheb.m
+++ PIETOOLS/PIESIM/PIESIM_routines/Discretization_Of_Operators/PIESIM_4PI2Mat_cheb.m
@@ -8,11 +8,14 @@
 % Rop - 4PI operator 
 % p - vector of dimension 1xns -
 % a "degree of smoothness" structure for 3PI, see Peet & Peet 2021 paper
-% flag = 0 if a structure has an empty right side (for Tw, Tu, B1 and B2
+%
+% flag = 0 if a structure has only the first row and is acting on disturbances or control inputs (for D11, D12,
+% D21, D22 operators)
+% flag = 1 if a structure is a full operator acting on disturbances or control inputs (for Tw, Tu, B1 and B2
 % operators)
-% flag = 1 if a structure is a full 4PI operator (for A and T)
-% flag = 2 if a structure has an empty bottom row (for C1, C2
-% operators)
+% flag = 2 if a structure has only the first row and acts on the PDE + ODE states (for C1, C2 operators)
+% flag = 3 if a structure is a full operator acting on the PDE+ODE states (for A and T)
+%
 %
 % Outputs:
 % A - square discretization matrix for PIE solution
@@ -26,53 +29,85 @@
 % authorship, and a brief description of modifications
 %
 % Initial coding YP  - 5_31_2021
-% YP - added flag = 2 and corresponding discretizations - now supports
-% computing of observed and regulated outputs
+% YP - added functionality to support infinite-dimensional disturbances
+% through parser - 6_1_2025
 
 function [A, A_nonsquare]=PIESIM_4PI2Mat_cheb(N, Rop, p, flag)
 
-% Pblock: size no x no
+% Iniatilize the matrix blocks of the full PI operator as empty
+ Pblock=[];
+ Q1block=[];
+ Q2block=[];
+ Rblock=[];
+
+ Q2block_nonsquare=[];
+ Rblock_nonsquare=[];
+
+% Pblock: size nscalar1 x nscalar2
 Pblock=double(Rop.P);
 
-% Q1block: size no x n0(N+1) n1N n2(N-1)
+% Q1block: size nscalar1 x nspat2
 
-if (flag==1)||(flag==2)
-    Q1block=PIESIM_PI2Mat_cheb_opint_discretize(N, Rop.Q1, p);
+
+if (size(Rop.Q1)~=0)
+
+if(flag<=1)
+pcol=zeros(size(Rop.Q1,2),1);
+else
+pcol=p;
 end
 
+Q1block=PIESIM_PI2Mat_cheb_opint_discretize(N, Rop.Q1, pcol);
+end
 
-% Q2block: size n0(N+1) n1N n2(N-1) x no
-% Rblock: size  n0(N+1) n1N n2(N-1) x n0(N+1) n1N n2(N-1)
+if (mod(flag,2)==1)
+
+% Q2block: size nspat1 x nscalr2
+
+% Q2block_nonsquare: nspat1_full x nscalar2
+
+
+if (size(Rop.Q2)~=0)
 
 if (nargout==1)
-    if flag~=2
-        Q2block= PIESIM_Poly2Mat_cheb(N, Rop.Q2, p);
-    end
-if (flag==1)
-    Rblock=PIESIM_3PI2Mat_cheb(N, Rop.R, p);
-end
+ Q2block= PIESIM_Poly2Mat_cheb(N, Rop.Q2, p);
 else
-% Q2block_nonsquare: size n0n1n2(N+1)^3 x no
-% Rblock_nonsquare: size  n0n1n2(N+1)^3 x n0(N+1) n1N n2(N-1)
-if flag~=2
 [Q2block, Q2block_nonsquare]= PIESIM_Poly2Mat_cheb(N, Rop.Q2, p);
 end
-if (flag==1)
-[Rblock, Rblock_nonsquare]=PIESIM_3PI2Mat_cheb(N, Rop.R, p);
-end
+
+end % size(Rop.Q2)~=0
+
+
+% Rblock: size  nspat1 x nspat2
+
+
+if (size(Rop.R.R0,2)~=0)
+
+if(flag<=1)
+pcol=zeros(size(Rop.R.R0,2),1);
+else
+pcol=p;
 end
 
-if (flag==1)
+if (nargout==1)
+Rblock=PIESIM_3PI2Mat_cheb(N, Rop.R, p, pcol);
+else
+[Rblock, Rblock_nonsquare]=PIESIM_3PI2Mat_cheb(N, Rop.R, p, pcol);
+end
+
+end % size(Rop.R.R0,2)~=0
+
+end % mod(flag,2)==1
+
+
+
+if (mod(flag,2)==1)
 A=[Pblock Q1block; Q2block Rblock];
-
 if (nargout>1)
     A_nonsquare=[Pblock Q1block; Q2block_nonsquare Rblock_nonsquare];
 end
-
-elseif (flag==2)
-    A=[Pblock, Q1block];
 else
-    A=[Pblock; Q2block];
+    A=[Pblock, Q1block];
 end
     
 
