7f2c6f002dde70ab195e7db37979bc56e0bbbc39
Declan <91631433+djagt@users.noreply.github.com>
2026-01-11 20:29:16 -0700

    Update get_quadratic_form.m
    The function now supports conversion of 'dpvar' objects to the quadratic form as well.

--- PIETOOLS/ndopvar/get_quadratic_form.m
+++ PIETOOLS/ndopvar/get_quadratic_form.m
@@ -1,42 +1,53 @@
-function [C,Zd1,Zd2] = get_quadratic_form(P,var1,var2,deg1,deg2)
-% [C,ZD1,ZD2] = GET_QUADRATIC_FORM(P,VAR1,VAR2,DEG1,DEG2)
-% Compute coefficients C such that P(x,y)=(Im o Zd1(x))^T C (In o Zd2(y)),
+function [C,Zd1,Zd2,Zdec] = get_quadratic_form(P,var1,var2,dvars,deg1,deg2)
+% [C,ZD1,ZD2,ZDEC] = GET_QUADRATIC_FORM(P,VAR1,VAR2,DVARS,DEG1,DEG2)
+% Compute coefficients C such that 
+%   P(x,y;z)=(Im o Zd1(x))^T (Ik o [1;dvars])^T C (In o Zd2(y)),
 % where x is var1 and y is var2, [m,n] are the dimensions of P,
 % o denotes the Kronecker product, and
 %   Zd1(x) = Zd1_{1}(x1) o Zd1_{2}(x2) o ... o Zd1_{p}(xp)
 % for Zd1_{i}(xi) the vector of monomials of degree at most deg1(i) in xi;
 % INPUTS
-% - R:      m x n 'polynomial' class object in variables var1 and/or var2,
-%           of degree at most d1 in var1 and d2 in var2 (not cumulative)
-% - var1:   p x 1 array of type 'polynomial' ('pvar') or 'cellstr'
-%           representing the variables x = (x1,...,xp);
-% - var2:   q x 1 array of type 'polynomial' ('pvar') or 'cellstr'
-%           representing the variables y = (y1,...,yp);
-% - deg1:   p x 1 array of integers specifying the maximal degree of the 
+% - P:      m x n 'dpvar' class object in variables var1 and/or var2,
+%           of degree at most d1 in var1 and d2 in var2 (not cumulative);
+% - var1:   p1 x 1 array of type 'polynomial' ('pvar') or 'cellstr'
+%           representing the variables x = (x1,...,xp1);
+% - var2:   p2 x 1 array of type 'polynomial' ('pvar') or 'cellstr'
+%           representing the variables y = (y1,...,yp2);
+% - dvars:  q x 1 array of type 'dpvar' or 'cellstr' representing the names
+%           of the decision variables
+% - deg1:   p1 x 1 array of integers specifying the maximal degree of the 
 %           monomials in each variable xi to use in the basis Zd1;
-% - deg2:   q x 1 array of integers specifying the maximal degree of the 
+% - deg2:   p2 x 1 array of integers specifying the maximal degree of the 
 %           monomials in each variable yi to use in the basis Zd2;
 %
 % OUTPUTS
-% - C:      m*d1 x n*d2 array of type 'sparse', 
-%           for d1 = deg1(1)*...*deg1(p),
+% - C:      m*r1*(q+1) x n*r2 array of type 'sparse', 
+%           for r1 = (deg1(1)+1)*...*(deg1(p1)+1),
 %           specifying the coefficients representing P in quadratic form.
-% R(x,y)=(Im o Zd(x))^T C (In o Zd(y))
+% - Zd1:    m*r1 x m array of type 'polynomial' representing the monomial
+%           basis (Im o Zd1(x));
+% - Zd2:    n*r2 x n array of type 'polynomial' representing the monomial
+%           basis (In o Zd2(y));
+% - Zdec:   m*r1*(q+1) x m*r1 'cellstr' specifying the vector of decision 
+%           variables, (Ik o [1;dvars]).
 
 
 
 %% % Check the inputs
 % Check the input polynomial.
 if isa(P,'double')
-    P = polynomial(P);
-elseif ~isa(P,'polynomial')
-    error("Input polynomial should be specified as object of type 'polynomial'")
+    P = dpvar(P);
+elseif isa(P,'polynomial')
+    P = poly2dpvar(P);  % <-- ideally, we would not perform this conversion
+elseif ~isa(P,'dpvar')
+    error("Input polynomial should be specified as object of type 'dpvar' or 'polynomial'")
 end
 % Extract degrees and coefficients
 [m,n] = size(P);
 Pvars = P.varname;
+Pdvars = P.dvarname;
 Pdegs = P.degmat;
-coeffs = P.coeff;
+coeffs = P.C;
 
 % Check the primary variables.
 if isa(var1,'char')
@@ -65,14 +76,31 @@ elseif isa(var2,'polynomial')
         var2_name(ii) = var2(ii).varname;
     end
     var2 = var2_name;
-elseif ~iscellstr(var2) && ~isempty(var2);
+elseif ~iscellstr(var2) && ~isempty(var2)
     error("Variables should be specified as array of 'pvar' objects or 'cellstr'.")
 end
 var1 = var1(:);         var2 = var2(:);
 nvars1 = numel(var1);   nvars2 = numel(var2);
+% Check the decision variables
+if isa(dvars,'char')
+    dvars = {dvars};
+elseif isa(dvars,'dpvar')
+    dvars_name = cell(numel(dvars),1);
+    for ii=1:numel(dvars)
+        dvar_ii = combine(dvars(ii));
+        if ~isscalar(dvar_ii.dvarname) || nnz(dvar_ii.C)>1 || nonzeros(dvar_ii.C)~=1
+            error("Decision variables should be specified as a qx1 array of individual 'dpvar' objects.")
+        end
+        dvars_name(ii) = dvar_ii.dvarname;
+    end
+    dvars = dvars_name;
+elseif ~iscellstr(dvars) && ~isempty(dvars)
+    error("Decision variables should be specified as array of 'dpvar' objects or 'cellstr'.")
+end
+dvars = dvars(:);
 
 % Check the degrees
-if nargin<=3
+if nargin<=4
     if isempty(Pdegs)
         deg1 = zeros(size(var1));
     else
@@ -85,7 +113,7 @@ elseif numel(deg1)~=numel(var1)
 end
 deg1 = deg1(:);
 d1 = prod(deg1+1);
-if nargin<=4
+if nargin<=5
     if isempty(Pdegs)
         deg2 = zeros(size(var2));
     else
@@ -101,15 +129,22 @@ d2 = prod(deg2+1);
 
 % Check that P depends only on the specified variables
 nvars = nvars1 + nvars2;
-if numel(Pvars)>nvars || any(~ismember(Pvars,[var1;var2]))
+if numel(Pvars)>0 && (numel(Pvars)>nvars || any(~ismember(Pvars,[var1;var2])))
     error("Input polynomial can depend only on the specified variables.")
 end
+q = numel(dvars);       q_P = numel(Pdvars);
+if q_P>0 && (q_P>q || any(~ismember(Pdvars,dvars))) 
+    error("Input object can depend only on the specified decision variables.")
+end
+
+
 
 
 %% % Compute the coefficient matrix C
 
-% Re-order the variable names of P to match var1 and var2
-Pdegs_new = zeros(size(Pdegs,1),nvars);
+% First, reorder the variable names of P to match var1 and var2
+nZ = size(Pdegs,1);
+Pdegs_new = zeros(nZ,nvars);
 vars = [var1;var2];
 for ii=1:nvars
     var_idx = ismember(Pvars,vars(ii));
@@ -123,55 +158,65 @@ if any(max(Pdegs1,[],1)>deg1') || any(max(Pdegs2,[],1)>deg2')
     error("Degree of monomials exceeds specified maximal degree.")
 end
 
+% Also check in what order the decision variables defining P appear in
+% dvars:
+% Pdvars = dvars(new2old_idcs)
+new2old_idcs = zeros(q_P,1);
+for ii=1:q_P
+    new2old_idcs(ii) = find(ismember(dvars,Pdvars(ii)),1,'first');
+end
+new2old_idcs = [1;new2old_idcs+1];      % account for constant term
 
-% Set the row and column number in the matrix-valued P associated with each
-% column of P.coeff
-r_nums = repmat((1:m),[1,n]);
-c_nums = kron((1:n),ones(1,m));
+% Now, all coefficients in C come from a coefficient in P.coeff.
+% --> extract the nonzero coefficient from P.coeff, as well as the row and
+%       column numbers in P.coeff in which they appear.
+[r_idcs_old,c_idcs_old,Cvals] = find(coeffs);
+
+% We need to determine which row and column each element of Cvals appears
+% in the new C matrix. 
+
+% Determine to which row in P each nonzero element in coeffs belongs
+r_nums = ceil(r_idcs_old(:)/(q_P+1));
+% Determine to which decision variable in P each nonzero element belongs
+Pdvar_nums = r_idcs_old(:) - (r_nums-1)*(q_P+1);
+% Determine to which column in P each nonzero element belongs
+c_nums = ceil(c_idcs_old(:)/nZ);
+% Determine to which monomial in P each nonzero element belongs
+Z_nums = c_idcs_old(:) - (c_nums-1)*nZ;
+
 % Set the number of rows and columns in a block in C associated to the same
 % row and column number in P:
 % rows r_plus(i)+1:r_plus(i+1) in C correspond to row r_nums(i) in P
-r_plus = (r_nums-1).*d1;
-c_plus = (c_nums-1).*d2;
-% Set the number of rows and columns in a block in C associated to 
-% the same power of variable ii:
+r_shift1 = (r_nums-1).*d1*(q+1);
+c_shift1 = (c_nums-1).*d2;
+
+% Set the number of rows in a block in C associated to the same power of 
+% variable ii:
 % rows 1 through nrows(ii) all involve the same power of var1(ii);
-% columns 1 through ncols(ii) all involve the same power of var2(ii);
 if isempty(deg1)
     nrows = zeros(0,1);
 else
-    nrows = cumprod(flipud(deg1+1));
-    nrows = flipud([1;nrows(1:end-1)]);
+    nrows = cumprod(flipud([deg1+1;q+1]));
+    nrows = flipud([nrows(1:end-1)]);
 end
+r_shift2 = Pdegs1*nrows;
+r_idcs = r_shift1 + r_shift2(Z_nums) + new2old_idcs(Pdvar_nums);
+
+% Set the number of columns in a block in C associated to the same power of 
+% variable ii:
+% columns 1 through ncols(ii) all involve the same power of var2(ii);
 if isempty(deg2)
     ncols = zeros(0,1);
 else
     ncols = cumprod(flipud(deg2+1));
     ncols = flipud([1;ncols(1:end-1)]);
 end
+c_shift2 = Pdegs2*ncols;
+c_idcs = c_shift1 + c_shift2(Z_nums) +1;
 
-% Extract the nonzero coefficients from P.coeff, and establish the
-% corresponding row and column numbers in the matrix C
-Cvals = [];
-r_idcs = [];
-c_idcs = [];
-for ii=1:size(coeffs,1)
-    % Get the coefficients associated with the iith monomial.
-    Cvals_ii = coeffs(ii,:);
-    is_nonzero = Cvals_ii~=0;
-    Cvals_ii = Cvals_ii(is_nonzero);
-    Cvals = [Cvals; Cvals_ii(:)];
-    % Get the row numbers in C of each of the coefficients.
-    Pdegs1_ii = Pdegs1(ii,:);
-    r_idcs_ii = r_plus(is_nonzero) + Pdegs1_ii*nrows + 1;
-    r_idcs = [r_idcs; r_idcs_ii(:)];
-    % Get the column numbers in C of each of the coefficients.
-    Pdegs2_ii = Pdegs2(ii,:);
-    c_idcs_ii = c_plus(is_nonzero) + Pdegs2_ii*ncols + 1;
-    c_idcs = [c_idcs; c_idcs_ii(:)];
-end
+
 % Declare the matrix C
-C = sparse(r_idcs,c_idcs,Cvals,m*d1,n*d2);
+C = sparse(r_idcs,c_idcs,Cvals,m*d1*(q+1),n*d2);
 if nargout==1
     return
 end
@@ -188,5 +233,7 @@ for ii=1:nvars2
     Zd_ii = monomials(pvar(var2{ii}),0:deg2(ii));
     Zd2 = kron(Zd2,Zd_ii(:));
 end
+k = size(Zd1,1);
+Zdec = dpvar(eye(k*(q+1)),zeros(1,0),{},dvars,[k,k*(q+1)])';
 
 end
