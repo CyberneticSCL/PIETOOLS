c01466ab380137dd615ed63d4898ffac93c22e09
Carl Richardson <carlrichardson@gmail.com>
2026-01-14 14:54:56 -0700

    commit test

--- PIETOOLS/ndopvar/@ndopvar/scalar.m
+++ PIETOOLS/ndopvar/@ndopvar/scalar.m
@@ -3,7 +3,7 @@ function Pop = scalar(alpha,Pop)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Pop = scalar(alpha,Pop) returns the 'ndopvar' object representing 
 % the scalar product alpha*Pop for the PI operator defined by Pop.
-% Date: 1/14/26
+% Date: 1/15/26
 % Version: 1.0
 % 
 % INPUT
