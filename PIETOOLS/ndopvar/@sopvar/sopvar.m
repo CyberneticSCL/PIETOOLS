1c3157f53698755028d178dfada42d17b5334971
Sachin Shivakumar <sshivakumar@lanl.gov>
2026-01-20 09:05:34 -0700

    Moved sopvar and quadpoly to ndopvar folder for now.

--- PIETOOLS/ndopvar/@sopvar/sopvar.m
+++ PIETOOLS/ndopvar/@sopvar/sopvar.m
@@ -1,14 +1,14 @@
 classdef (InferiorClasses={?polynomial,?dpvar})sopvar
-    % Represents PI maps from L_2^p[Si,...Sn] x L_2^q[Sj,...,Sm]
+    % Represents PI maps from L_2^p[Si,...Sn] to L_2^q[Sj,...,Sm]
     % note [Si,...,Sn] may not have all indices from i to n, likewise
     % for j to m.
     properties
-        vars_in = {};
-        vars_out = {};
-        dims = [1,1];
+        vars_in = {};   % cell string  {'Si',...,'Sn'}
+        vars_out = {};  % cell strings {'Sj',...,'Sm'}
+        dims = [1,1];  % vector-space dimension [q,p]
         params = {quadPoly(0,[],[],[1,1],{},{})};
-        % each value in the cell is a quadPoly
-        % for maps to and from \R spaces, add an extra dimension?
+        % each value in the params cell is a quadPoly
+        % for maps to and from \R spaces, add an extra dimension, s0?
     end
 
     methods
