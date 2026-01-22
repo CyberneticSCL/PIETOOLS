011da68c1ce9a614adb092176c2359befa1d624e
Declan <91631433+djagt@users.noreply.github.com>
2026-01-21 17:08:41 -0700

    Update quad2lin.m
    This function can convert a distributed polynomial in quadratic format to the linear format.

--- PIETOOLS/polyopvar/sosprogramming/quad2lin.m
+++ PIETOOLS/polyopvar/sosprogramming/quad2lin.m
@@ -12,14 +12,42 @@ function Vx = quad2lin(Pmat,ZopL,ZxL,ZopR,ZxR)
 %           operators acting on the distributed monomials ZxL;
 % - ZxL:    d1 x 1 'polyopovar' object representing a basis of distributed
 %           monomials;
-% - ZopR:   n x d1 'tensopvar' object representing the coefficient
+% - ZopR:   n x d2 'tensopvar' object representing the coefficient
 %           operators acting on the distributed monomials ZxR. Defaults to
 %           ZopL if not specified;
+% - ZxR:    d2 x 1 'polyopovar' object representing a basis of distributed
+%           monomials;
 %
 % OUTPUTS
 % - Vx:     1 x 1 'polyopvar' object representing the function
 %               V(x) = <ZopL*ZL(x), Pmat*ZopR*ZR(x)>_{L2}
 %           in the linear format;
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% PIETOOLS - quad2lin
+%
+% Copyright (C) 2026 PIETOOLS Team
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% If you modify this code, document all changes carefully and include date
+% authorship, and a brief description of modifications
+%
+% DJ, 01/21/2026: Initial coding
 
 
 % Assume a symmetric operator if only one half is specified.
@@ -45,11 +73,18 @@ old2new_idcs = M*(1:size(degmat_lin,1))';               % degmat_full = degmat_l
 nZ_lin = size(degmat_lin,1);
 
 % Get the dimensions of the different operators
-blkdim = zeros(nZL,1);
+blkdimL = zeros(nZL,1);
 for ii=1:nZL
-    blkdim(ii) = ZopL(ii,ii).dim(1);
+    tmp = ZopL(ii,ii);
+    blkdimL(ii) = tmp.dim(1);
 end
-blkdim_cum = [0;cumsum(blkdim)];
+blkdimL_cum = [0;cumsum(blkdimL)];
+blkdimR = zeros(nZR,1);
+for ii=1:nZR
+    tmp = ZopR(ii,ii);
+    blkdimR(ii) = tmp.dim(1);
+end
+blkdimR_cum = [0;cumsum(blkdimR)];
 
 Kop = tensopvar();
 Kop.ops = cell(1,nZ_lin);
@@ -63,7 +98,7 @@ for ii=1:nZL
         Zop_jj = ZopR(jj,jj);
         % Extract the block of decision variables acting on monomials Zi
         % and Zj
-        Pij = Pmat(blkdim_cum(ii)+1:blkdim_cum(ii+1), blkdim_cum(jj)+1:blkdim_cum(jj+1));
+        Pij = Pmat(blkdimL_cum(ii)+1:blkdimL_cum(ii+1), blkdimR_cum(jj)+1:blkdimR_cum(jj+1));
         % Convert to the linear format
         Kij = quad2lin_term(Zop_ii,Pij,Zop_jj);
         % We need to re-order variables to account for the orderof the 
