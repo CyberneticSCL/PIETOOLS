219fd6c31abd0159871523690f2a89479a5922d1
Declan <91631433+djagt@users.noreply.github.com>
2026-02-11 20:05:04 -0700

    Update int_delta.m
    Minor bugfix.

--- PIETOOLS/polyopvar/@tensopvar/int_delta.m
+++ PIETOOLS/polyopvar/@tensopvar/int_delta.m
@@ -159,11 +159,7 @@ end
 % For each possible ordering of the variables (r1,t1,...,td,r2), compute 
 % the kernel in the associated term in the functional
 n_ords = size(omat,1);
-if isa(Kfun,'dpvar')
-    Cfun = dpvar(zeros(pdim,ndim*n_ords));
-else
-    Cfun = polynomial(zeros(pdim,ndim*n_ords));
-end
+Cfun = polynomial(zeros(pdim,ndim*n_ords));
 for i=1:n_ords
     param_i = 0;
     % Establish in which position the variable s is placed
