b8fad4850ced334b557a64f80bf70e202c6172fb
Declan <91631433+djagt@users.noreply.github.com>
2026-02-13 17:09:01 -0700

    Create mtimes_pdes.m
    Initial step towards supporting nonlinear terms in PDEs. Still under construction.

---
+++ PIETOOLS/converters/@pde_struct/mtimes_pdes.m
@@ -0,0 +1,201 @@
+function PDE_out = mtimes_pdes(PDE1,PDE2)
+% PDE_OUT = MTIMES_PDES(PDE1,PDE2) computes the product of two terms that
+% may appear in a PDE.
+%
+% INPUTS
+% - PDE1:   'pde_struct' object representing a single term to be used in a
+%           PDE;
+% - PDE2:   'pde_struct' object representing a single term to be used in a
+%           PDE;
+%
+% OUTPUTS
+% - PDE_out:    'pde_struct' object representing the product of the two
+%               input terms;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright (C)2026  PIETOOLS Team
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% If you modify this code, document all changes carefully and include date
+% authorship, and a brief description of modifications
+%
+% DJ, 02/13/2026: Initial coding;
+
+% Make sure the inputs are 'pde_struct' objects
+if ~isa(PDE1,'pde_struct') || ~isa(PDE2,'pde_struct')
+    error("The factors to multiply should be specified as 'pde_struct' objects.")
+end
+% Make sure the input corresponds to a single state/input/output
+% or set of terms.
+[is_pde_var1,obj1] = is_pde_var(PDE1);
+[is_pde_var2,obj2] = is_pde_var(PDE2);
+if (~is_pde_var1 && ~is_pde_term(PDE1)) || (~is_pde_var2 && ~is_pde_term(PDE2))
+    error("The factors to multiple should correspond to individual terms in the PDE.")
+end
+% Convert the PDE variables to terms
+if is_pde_var1
+    PDE1 = var2term(PDE1,obj1);
+end
+if is_pde_var2
+    PDE2 = var2term(PDE2,obj2);
+end
+% Express the terms in terms of the same set of states, inputs, and
+% outputs
+[PDE1,PDE2] = pde_common_basis(PDE1,PDE2);
+
+eq_size_list1 = size(PDE1,'free','vec_size');
+eq_size_list2 = size(PDE2,'free','vec_size');
+if numel(eq_size_list1)~=numel(eq_size_list2)
+    error("Number of rows of terms must match for elementwise multiplication.")
+elseif any( eq_size_list1(eq_size_list2>1)>1 & eq_size_list1~=eq_size_list2 )
+    error("Row dimensions of terms must match for elementwise multiplication.")
+end
+eq_size_out = max(eq_size_list1,eq_size_list2);
+
+% Construct a structure representing the product
+PDE_out = PDE1;
+for i=1:numel(PDE_out.free)
+    % Set the ith row of terms in the PDE
+    eq_i = PDE_out.free{i};
+    eq1_i = PDE1.free{i};
+    eq2_i = PDE2.free{i};
+    % The product will depend on the variables of both factors
+    vars1 = eq1_i.vars;
+    vars2 = eq2_i.vars;
+    new_varnames = unique([vars1.varname;vars2.varname]);
+    eq_i.vars = polynomial(new_varnames);
+    % The size of the elementwise product will be the size of the factors
+    eq_i.size = eq_size_out(i);
+    % The terms in the ith row will be the products of the terms in 1 and 2
+    eq_i.term = cell(1,numel(eq1_i.term)*numel(eq2_i.term));
+    for j=1:numel(eq_i.term)
+        % To represent the product of two terms, we concatentate the
+        % struct objects
+        [trm_num1,trm_num2] = ind2sub([numel(eq1_i.term),numel(eq2_i.term)],j);
+        eq_i.term = times_term(eq1_i.term{trm_num1},eq2_i.term{trm_num2},PDE_out);
+    end
+    PDE_out.free{i} = eq_i;
+end
+
+end
+
+
+
+function term_out = times_term(term1,term2,PDE)
+% Return a structure representing the product of two terms in a PDE
+
+% Check whether term1 involves a state or input variable
+if isfield(term1,'x')
+    obj1 = 'x';
+elseif isfield(term1,'u')
+    obj1 = 'u';
+elseif isfield(term1,'w')
+    obj1 = 'w';
+else
+    error("Multiplication with output signals is not supported.")
+end
+obj1_num = term1.(obj1);
+obj1_size = PDE.(obj1){obj1_num}.size;
+obj1_vars = PDE.(obj1){obj1_num}.vars;
+
+% Check whether term1 involves a state or input variable
+if isfield(term2,'x')
+    obj2 = 'x';
+elseif isfield(term2,'u')
+    obj2 = 'u';
+elseif isfield(term2,'w')
+    obj2 = 'w';
+else
+    error("Multiplication with output signals is not supported.")
+end
+obj2_num = term2.(obj2);
+obj2_size = PDE.(obj1){obj2_num}.size;
+obj2_vars = PDE.(obj2){obj2_num}.vars;
+
+% Add potential missing object field to each term
+if ~strcmp(obj1,obj2)
+    term1.(obj2) = [];
+    term2.(obj1) = [];
+end
+
+% Preclude delay in the terms
+if isfield(term1,'delay')
+    if ~isequal(term1.delay,0)
+        error("Multiplication of delayed signals is not supported.")
+    else
+        term1 = rmfield(term1,'delay');
+    end
+end
+if isfield(term2,'delay')
+    if ~isequal(term2.delay,0)
+        error("Multiplication of delayed signals is not supported.")
+    else
+        term2 = rmfield(term2,'delay');
+    end
+end
+
+% Check if the number of rows in the terms matches
+if isfield(term1,'C')
+    if ~isscalar(term1.C)
+        obj1_size = size(term1.C,1);
+    end
+else
+    term1.C = eye(obj1_size);
+end
+if isfield(term2,'C')
+    if ~isscalar(term2.C)
+        obj2_size = size(term2.C,1);
+    end
+else
+    term2.C = eye(obj2_size);
+end
+% Augment scalar value to vector
+if obj2_size>1 && obj1_size==1
+    term1.C = ones(obj2_size,1)*term1.C;
+elseif obj1_size>1 && obj2_size==1
+    term2.C = ones(obj1_size,1)*term2.C;
+elseif obj1_size~=obj2_size
+    error("Row dimensions of terms must match for elementwise multiplication.")
+end
+
+% Make sure the derivative to be taken is specified
+if ~isfield(term1,'D')
+    term1.D = zeros(1,size(obj1_vars,1));
+end
+if ~isfield(term2,'D')
+    term2.D = zeros(1,size(obj2_vars,1));
+end
+% Make sure the location at which to evaluate is specified
+if ~isfield(term1,'loc')
+    term1.loc = obj1_vars(:,1)';
+end
+if ~isfield(term2,'loc')
+    term2.loc = obj2_vars(:,1)';
+end
+% Make sure the integral to take is specified
+if ~isfield(term1,'I')
+    term1.I = cell(size(obj1_vars,1),1);
+end
+if ~isfield(term2,'I')
+    term2.I = cell(size(obj2_vars,1),1);
+end
+
+% Finally, just concatenate the two structures
+term_out = [term1,term2];
+
+end
