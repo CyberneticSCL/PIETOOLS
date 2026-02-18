4255b240f6497802a69c7ce4111f62c4b6d9cd53
Declan <91631433+djagt@users.noreply.github.com>
2026-02-17 17:10:55 -0700

    Create power functions for PDE.
    They don't do anything yet, but once finished they will allow for the elementwise power of PDE variables to be taken.

---
+++ PIETOOLS/converters/@pde_struct/power.m
@@ -0,0 +1,16 @@
+function [outputArg1,outputArg2] = power(inputArg1,inputArg2)
+%POWER Summary of this function goes here
+%   Detailed explanation goes here
+arguments (Input)
+    inputArg1
+    inputArg2
+end
+
+arguments (Output)
+    outputArg1
+    outputArg2
+end
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
