56f9b63685d1264fa1e1d28435c13a99f042a6bd
Talitsky <talitsky@gmail.com>
2026-03-17 14:02:49 -0700

    First version of left/rightshiftMonomials
    It requires additional testing.

---
+++ PIETOOLS/ndopvar/@sopvar/private/leftshiftMonomoials_AT.m
@@ -0,0 +1,74 @@
+function [Zs_new, ns_new, CL] = leftshiftMonomoials_AT(Lbeta, Zs_mult, var_Zs)
+% The function compute CL and Zs_new such that 
+% Zs_mult^T Lbeta = Zs_new^T CL
+% INPUTs
+% Lbeta -- quadpoly
+% Zs_mult -- vector of monomials
+% var_Zs -- names of the monomials
+
+
+P_subs1 = Lbeta;
+ns = Lbeta.ns;
+nt = Lbeta.nt;
+dim = Lbeta.dim;
+% first we move all monomials for Lbeta to the left
+nt_RENAMED = cellfun(@(name) char(strcat("<subs_>", name)), nt, 'UniformOutput', false);
+    
+
+for var_id = 1:length(nt)
+    name_OLD = nt{var_id};
+    name_NEW = nt_RENAMED{var_id};
+    P_subs1 = var_swap(P_subs1, name_NEW, name_OLD);%first 7 symbols include <subs_>
+end    
+
+ns_new = P_subs1.ns;
+Zs_new = P_subs1.Zs;
+nt_new = P_subs1.nt;
+Zt_new = P_subs1.Zt;
+C_new  = P_subs1.C;
+
+% remove subs from varnames
+ns_to_renamed = cellfun(@(name) char(eraseBetween(string(name), "<", ">",'Boundaries','inclusive')),...
+    ns_new, 'UniformOutput', false); % names to rename
+
+% Then combine two set of monomials Zs_mult and P.Zs
+Zs_full =  table2cell([cell2table(Zs_mult), cell2table(Zs_new)]);
+ns_full =  table2cell([cell2table(var_Zs), cell2table(ns_to_renamed)]);
+
+% Given Zs_full, ns_full, we sort the variable names as in combine function
+P_subs1 = quadPoly(C_new, Zs_full, {[0]}, [1, dim(2)], ns_full, {'<empty_var>'}, 0)   ;
+P_subs1 = combine(P_subs1);
+
+% finally Zs, ns may have repeated var names
+Zt_new = P_subs1.Zt;
+Zs_new = P_subs1.Zs;
+nt_new = P_subs1.nt;
+ns_new = P_subs1.ns;
+CL = P_subs1.C;
+dim_new = P_subs1.dim;
+
+% merge monomial degrees for repeated ns
+is_repeated_in_ns = cellfun(@isequal, ns_new(1:end-1), ns_new(2:end));
+while sum(is_repeated_in_ns) > 0
+    idx_to_merge_monomials = find(is_repeated_in_ns);% find repeated var names
+    idx_to_merge_monomials = idx_to_merge_monomials(1);
+    ns_new{idx_to_merge_monomials+1} = []; % delete repeated var name
+    Zs_new_idx=  Zs_new{idx_to_merge_monomials}; % choose repeated monoms
+    Zs_new_idx_p1=  Zs_new{idx_to_merge_monomials + 1};
+    Zs_new{idx_to_merge_monomials + 1} = []; % delete dublicate
+    % Define new monomial degrees
+    Zs_new_merged = kron(Zs_new_idx, ones(size(Zs_new_idx_p1))) ...
+                    + kron(ones(size(Zs_new_idx)), Zs_new_idx_p1);
+    Zs_new{idx_to_merge_monomials} = Zs_new_merged;
+    Zs_new = Zs_new(~cellfun(@isempty, ns_new)); % remove empty cells
+    ns_new = ns_new(~cellfun(@isempty, ns_new)); % remove empty cells
+    % check if any repeated var names
+    if length(ns_new) > 1
+        is_repeated_in_ns = cellfun(@isequal, ns_new(1:end-1), ns_new(2:end));
+    else
+        is_repeated_in_ns = {0};
+    end
+end
+
+end
+
---
+++ PIETOOLS/ndopvar/@sopvar/private/rightshiftMonomials_AT.m
@@ -0,0 +1,73 @@
+function [Zt_new,nt_new, CR] = rightshiftMonomials_AT(Ralpha,Zs_mult, var_Zs)
+% The function compute CL and Zs_new such that 
+% Zs_mult^T Ralpha =  CR Zs_new
+% INPUTs
+% Ralpha -- quadpoly
+% Zs_mult -- vector of monomials
+% var_Zs -- names of the monomials
+
+
+
+P_subs1 = Ralpha;
+ns = Ralpha.ns;
+nt = Ralpha.nt;
+dim = Ralpha.dim;
+% first we move all monomials for Ralpha to the right side
+ns_RENAMED = cellfun(@(name) char(strcat("<subs_>", name)), ns, 'UniformOutput', false);
+    
+
+for var_id = 1:length(ns)
+    name_OLD = ns{var_id};
+    name_NEW = ns_RENAMED{var_id};
+    P_subs1 = var_swap(P_subs1, name_OLD, name_NEW);%first 7 symbols include <subs_>
+end    
+
+ns_new = P_subs1.ns;
+Zs_new = P_subs1.Zs;
+nt_new = P_subs1.nt;
+Zt_new = P_subs1.Zt;
+C_new  = P_subs1.C;
+
+% remove <subs_> from varnames
+nt_to_renamed = cellfun(@(name) char(eraseBetween(string(name), "<", ">",'Boundaries','inclusive')),...
+    nt_new, 'UniformOutput', false); % names to rename
+
+% Then combine two set of monomials Zs_mult and P.Zs
+Zt_full =  table2cell([cell2table(Zs_mult), cell2table(Zt_new)]);
+nt_full =  table2cell([cell2table(var_Zs), cell2table(nt_to_renamed)]);
+
+% Given Zs_full, ns_full, we sort the variable names as in combine function
+P_subs1 = quadPoly(C_new, {[0]}, Zt_full,  [dim(1), 1], {'<empty_var>'}, nt_full, 0)   ;
+P_subs1 = combine(P_subs1);
+
+% finally Zt, nt may have repeated var names
+Zt_new = P_subs1.Zt; 
+nt_new = P_subs1.nt; 
+CR = P_subs1.C; 
+
+% merge monomial degrees for repeated ns
+is_repeated_in_nt = cellfun(@isequal, nt_new(1:end-1), nt_new(2:end));
+while sum(is_repeated_in_nt) > 0
+    idx_to_merge_monomials = find(is_repeated_in_nt);% find repeated var names
+    idx_to_merge_monomials = idx_to_merge_monomials(1);
+    nt_new{idx_to_merge_monomials+1} = []; % delete repeated var name
+    Zt_new_idx=  Zt_new{idx_to_merge_monomials}; % choose repeated monoms
+    Zs_new_idx_p1=  Zt_new{idx_to_merge_monomials + 1};
+    Zt_new{idx_to_merge_monomials + 1} = []; % delete dublicate
+    % Define new monomial degrees
+    Zs_new_merged = kron(Zt_new_idx, ones(size(Zs_new_idx_p1))) ...
+                    + kron(ones(size(Zt_new_idx)), Zs_new_idx_p1);
+    Zt_new{idx_to_merge_monomials} = Zs_new_merged;
+    Zt_new = Zt_new(~cellfun(@isempty, nt_new)); % remove empty cells
+    nt_new = nt_new(~cellfun(@isempty, nt_new)); % remove empty cells
+    % check if any repeated var names
+    if length(nt_new) > 1
+        is_repeated_in_nt = cellfun(@isequal, nt_new(1:end-1), nt_new(2:end));
+    else
+        is_repeated_in_nt = {0};
+    end
+end
+
+
+end
+
