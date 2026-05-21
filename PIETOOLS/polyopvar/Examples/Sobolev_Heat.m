411af097687e407ff7682c347dd76d8d57c858b9
Declan <91631433+djagt@users.noreply.github.com>
2026-05-20 20:44:32 -0700

    Update Sobolev tests.
    A stability test for the heat equation with quadratic nonlinearity has been added as well now. It works without the nonlinear term, but local stability with the nonlinearity cannot be certified.

--- PIETOOLS/polyopvar/Examples/Sobolev_Heat.m
+++ PIETOOLS/polyopvar/Examples/Sobolev_Heat.m
@@ -17,31 +17,186 @@ clear;  clear stateNameGenerator
 pvar   s t s_dum
 L = 1;
 a = 1;
-k = 1;
+k = 10;
+%k = a*pi^2/(4*L^2);
 dom = [0,L];
 x = pde_var(s,dom);
 
 % FULL PDE
-% PDE = [diff(x,t) == a*diff(x,s,2) + x^2 - (k/L)*int(x,s,dom(1),dom(2));
-%        subs(diff(x,s,1),s,dom(1))==0; subs(x,s,dom(2))==0];
+PDE = [diff(x,t) == a*diff(x,s,2) +x^2- (k/L)*int(x,s,dom(1),dom(2));
+       subs(diff(x,s,1),s,dom(1))==0; subs(x,s,dom(2))==0];
 
-% TEMP PDE FOR COMPUTING T.
-PDE = [diff(x,t) == a*diff(x,s,2) + x^2;
-        subs(diff(x,s,1),s,dom(1))==0; subs(x,s,dom(2))==0];
 
 % Script parameters
-
-
+R = sqrt(0.1); % Radius of ball in which to test stability
+R = 1;
+d = 1;    % degree of LF distributed monomial basis (will be doubled in LF).
+pdeg = 4; % degree of Zs monomials of positive P operator.
+use_L2_LF = false;   % Express the Lyapunov function in terms of an L2 norm of the PDE state
+use_L2_bnd = true;  % Enforce a lower bound on the LF in terms of the L2 norm of the PDE state
+use_L2_ball = false; % local test on L2 ball (if TRUE) or Sobolev ball (if false) of radius R.
+d_psatz2 = 1; % degree of distributed monomial in upper bound condition of LF derivative.
+eppos = 0.1; % coefficient in LF lower bound condition.
+q1_deg = 4;
+q2_deg = 4; % degree of monomials (not distributed) used to define the operator W2
+q3_deg = 5; % degree of monomials (not distributed) used to define the operator W3
+lam1_deg = 4; % degree of monomials (not distributed) used to define the operator lam1
+lam2_deg = 4; % degree of monomials (not distributed) used to define the operator lam2
 
 %% 1. Modelling PIE.
 
 % Convert to a PIE
-PIE = convert(PDE); % CANT HANDLE THE INTEGRAL TERM BUT CAN USE IT TO COMPUTE T.
+PIE = convert_PIETOOLS_PDE_nonlinear(PDE);
 Top = PIE.T;
 Top_opvar = ndopvar2dopvar(Top);
+Rop = dopvar2ndopvar(diff(Top_opvar,Top_opvar.var1,1,'pure'));
 f = PIE.f;
 x = polyopvar(f.varname,s,dom);
+x.C.ops{1}.ops{1}.C{2} = 0;
+x.C.ops{1}.ops{1}.C{3} = 0;
 
-% just need to add integral term to f?
-% d0 = dmonomials(x,0);
-% innerprod(d0,x,Top)
+
+%% 2. Setting up PIESOS program
+
+% Initialize PIESOS program structure.
+% dpvar gam
+prog = piesos_program(x); % gam
+
+% % Define as a minimization problem on the upper bound (gamma) of the LF 
+% % over the domain L_{2,R}.
+% prog = lpisetobj(prog,gam);
+
+%% 3. Construct LF
+% V(v) = <Z_d(v), Pop*Z_d(Tv)>; Z_d(v) = [v ... v^d]'; Pop = Zop^* Pmat Zop.
+
+% Declare the Lyapunov functional.
+disp(" --- Declaring the distributed SOS Lyapunov functional ---")
+
+if use_L2_LF
+    Tx = Top*x;
+    Vx = innerprod(Tx,Tx);
+else
+    % Construct monomial basis for Pop=P*Z 
+    Zmon1 = monomials(s,0:pdeg);
+    Zmon2 = monomials([s,s_dum],0:pdeg);
+    Zop = opvar();
+    Zop.R.R0 = [Zmon1;0*Zmon2;0*Zmon2];
+    Zop.R.R1 = [0*Zmon1;Zmon2;0*Zmon2];
+    Zop.R.R2 = [0*Zmon1;0*Zmon2;Zmon2];
+    Zop.var1 = s;
+    Zop.var2 = s_dum;
+    Zop.I = dom;
+    Z = dopvar2ndopvar(Zop);
+    
+    % P matrix (not enforcing positivity here)
+    [prog,Pcell] = sosquadvar(prog,{1},{1},size(Zop,1),1); % 1's restrict Pcell to matrix (not function)
+    
+    % Construct LF (up to degree 3).
+    Tx = Top*x;
+    Zx = Z*x;
+    Vx = innerprod(Tx,Zx,Pcell{1}');
+end
+
+
+%% Enforce the symmetry condition
+if ~use_L2_LF
+    Pop = Pcell{1}'*Zop;
+    prog.dom = dom;
+    prog.vartable = [s;s_dum];
+    prog = lpi_eq(prog,Pop'*Top_opvar-Top_opvar'*Pop);
+    prog.vartable = {'s'};
+end
+
+
+%% 4. Construct the derivative of the LF along the PIE
+disp(" --- Computing the derivative along the PIE ---")
+if use_L2_LF
+    dV = 2*innerprod(Tx,f);
+else
+    dV = 2*innerprod(Zx,f,Pcell{1});
+end
+
+
+%% 5. Define the specified local ball as a semialgebraic set.
+disp(" --- Declaring the local ball ---")
+if use_L2_ball
+    g = R^2 - innerprod(Tx,Tx); % L2 ball of radius R.
+else
+    Rx = Rop*x;
+    g = R^2 - innerprod(Rx,Rx); % Sobolev ball of radius R. - innerprod(Tx,Tx) 
+end
+
+
+%% 6. Define the lower bound on the LF (holds globally) and enforce constraint.
+
+if use_L2_bnd % LF upper bound dependent on ball.
+    V_low = Vx - eppos*innerprod(Tx,Tx);
+else
+    Rx = Rop*x;
+    V_low = Vx - eppos*innerprod(Rx,Rx);
+end
+
+% Enforce lower bound by defining a SOS distributed monomial
+disp(" --- Enforcing the bound on the Lyapunov functional ---")
+
+% Declare W1 >= 0
+Z_bnd_degmat = unique(floor(V_low.degmat./2),'rows'); % This is the degree of W1
+Z_bnd = polyopvar(f.varname,s,dom);
+Z_bnd.degmat = Z_bnd_degmat;
+Q1_opts.deg = q1_deg; % degree of monomials (not distributed) used to define the operator W1
+Q1_opts.exclude = [1,0,0]';
+Q1_opts.psatz = 0:1;
+[prog,W1,Q1mat,ZQ1op] = piesos_sosvar(prog,Z_bnd,Q1_opts);
+
+disp("  --  enforcing lower bound equality")
+prog = piesos_eq(prog,V_low-W1);
+
+
+%% 9. Define the upper bound on the LF derivative over the specified ball and enforce constraint.
+
+% Declare the SOS multiplier, lam2, then define bound.
+Zg2 = dmonomials(x,(1:d_psatz2));
+if d_psatz2>=1
+    lam2_opts.exclude = [1,0,0]';
+    lam2_opts.deg = lam2_deg; % degree of monomials (not distributed) used to define the operator lam2
+    lam2_opts.psatz = 0;
+    [prog,lam2] = piesos_sosvar(prog,Zg2,lam2_opts);
+    dV_up = -dV - lam2*g;
+else
+    dV_up = -dV;
+end
+
+% Enforce upper bound by defining a SOS distributed monomial.
+disp(" --- Enforcing negativity of the derivative ---")
+
+% Declare W3 >= 0
+ZQ_degmat = unique(floor(dV_up.degmat./2),'rows');
+ZQ = polyopvar(f.varname,s,dom);
+ZQ.degmat = ZQ_degmat;
+Q3_opts.deg = q3_deg; % degree of monomials (not distributed) used to define the operator W3
+Q3_opts.exclude = [0,0,0]';
+Q3_opts.psatz = 0:1;
+[prog,W3] = piesos_sosvar(prog,ZQ,Q3_opts); % DO WE NEED Q3MAT AND ZQ3OP??????????????????
+
+% Enforce dV = W3 >= 0
+disp("  --  enforcing upper bound equality on derivative")
+prog = piesos_eq(prog,dV_up-W3);
+
+
+%% 10. Solve the optimization program
+disp(" --- Solving the optimization program ---")
+sol_opts.simplify = true;
+prog_sol = piesos_solve(prog,sol_opts);
+
+
+% % Extract the solution
+sol_info = prog_sol.solinfo.info;
+if sol_info.pinf || sol_info.dinf || sol_info.numerr || abs(sol_info.feasratio-1)>0.1
+    disp("PIESOS program was not solved.")
+else
+    disp("PIESOS program was successfully solved.")
+    Vsol = piesos_getsol(prog_sol,Vx);
+    % gam_sol = piesos_getsol(prog_sol,gam);
+end
+
+
