5135d8f55a6ff9bf4c050e7bcd0af1753f658b83
Declan <91631433+djagt@users.noreply.github.com>
2026-05-27 19:25:37 -0700

    Create mtimes test scripts.
    So far, the test scripts keep producing errors when calling `mtimes`. It is possible that this due to the opvar2sopvar function not working properly, but I'm not sure.

---
+++ PIETOOLS/ndopvar/Testfolder/Test_mtimes_sopvar_1d.m
@@ -0,0 +1,32 @@
+
+% Declare the independent variables
+pvar s1 s1_dum
+var1 = s1;
+var2 = s1_dum;
+% Declare the domain
+dom = [0,1];
+% Declare the degree of the parameters in the independent variables
+deg1 = randi(3)-1;
+deg2 = randi(3)-1;
+% Declare the input and output dimensions of the operators
+dim1 = [0;1];
+dim2 = [0;1];
+dim3 = [0;1];
+
+% Generate random opvar objects of the desired dimensions
+Aop = rand_opvar([dim1,dim2],deg1,var1,var2,dom);
+Bop = rand_opvar([dim2,dim3],deg2,var1,var2,dom);
+
+% Compute the composition
+Cop = Aop*Bop;
+
+% Convert to sopvar
+Asop = opvar2sopvar(Aop);
+Bsop = opvar2sopvar(Bop);
+
+% Compute the sopvar composition
+Csop = Asop*Bsop;
+
+% Compare
+Cop_alt = sopvar2opvar(Csop);
+Cop_diff = clean_opvar(Cop - Cop_alt);
