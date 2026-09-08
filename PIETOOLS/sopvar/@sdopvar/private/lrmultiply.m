c66c218e79e175f06716e3beb4b7e14ba5e4010a
Talitsky <talitsky@gmail.com>
2026-09-08 09:56:18 -0700

    vectorization of lrmultiply and mtimes_AT
    mtimes_AT new lines 410-550

--- PIETOOLS/sopvar/@sdopvar/private/lrmultiply.m
+++ PIETOOLS/sopvar/@sdopvar/private/lrmultiply.m
@@ -4,6 +4,10 @@ function Cout = lrmultiply(L, C, R)
 % Cout = vec(L*C*R) 
 % where C, Cout are structs with fields A, B
 % and L/R are sparse matrices
+%
+% initial SS
+% vectorization AT 09/08/26
+%
 
 A = C.A;
 B = C.B;
@@ -14,11 +18,23 @@ Y = L*X*R;  % (R^T\otimes L)*A = vec(L*X*R), X = reshape(A)
 Aout = Y(:);
 
 Bout = sparse(size(B,1),size(L,1)*size(R,2));
-for i=1:size(B,1)  % row slicing is likely to me slow
-X = reshape(B(i,:)',size(L,2),size(R,1));
-Y = L*X*R;
-Bout(i,:) = Y(:);
-end
+
+% new implementation using kronecker product
+Rkron = kron(speye(size(L,2)), R);
+Lkron = kron(L, speye(size(R,1)));
+LRkron = Lkron*Rkron;
+Bout = B*LRkron;
+
+% Previous implementation
+% it was a loop over decision variables
+% for i=1:size(B,1)  % row slicing is likely to me slow
+%     X = reshape(B(i,:)',size(L,2),size(R,1));
+%     Y = L*X*R;
+%     Bout(i,:) = Y(:);
+% end 
+% if max(abs(Bout - Out), [], 'all') > 1.e-10
+%     error('ERROR')
+% end
 
 Cout = struct('A',Aout,'B', Bout);
 end
